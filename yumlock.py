5218e6bbc6d15ed807c073f83a15f929e089f1c7
Seth Vidal <skvidal@linux.duke.edu>
2003-01-17 21:58:00 +0000

    added yumlock from michael stenner for locking functionality made yum support lockfiles for root uses

---
+++ yumlock.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python -t
+
+#thanks to michael stenner for these straightforward lock and lock checking routines
+import os
+
+def lock(filename, contents='', mode=0777):
+    try:
+        fd = os.open(filename, os.O_EXCL|os.O_CREAT|os.O_WRONLY, mode)
+    except OSError, msg:
+        if not msg.strerror == "File exists": raise msg
+        return 0
+    else:
+        os.write(fd, contents)
+        os.close(fd)
+        return 1
+
+def unlock(filename):
+    os.unlink(filename)
