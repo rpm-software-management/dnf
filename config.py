25932bd410cf58f0ad65896b7b56dfaa6842cd69
Seth Vidal <skvidal@linux.duke.edu>
2003-10-15 01:17:45 +0000

    merging Ryan Tomayko's include= patch for the config file

--- config.py
+++ config.py
@@ -27,6 +27,7 @@ import failover
 import archwork
 import rpmUtils
 import progress_meter
+import urlgrabber
 
 from i18n import _
 
@@ -35,20 +36,13 @@ class yumconf:
 
     def __init__(self, configfile = '/etc/yum.conf'):
         self.cfg = ConfigParser.ConfigParser()
-        (s,b,p,q,f,o) = urlparse.urlparse(configfile)
-        if s in ('http', 'ftp','file'):
-            configfh = urllib.urlopen(configfile)
-            try:
-                self.cfg.readfp(configfh)
-            except ConfigParser.MissingSectionHeaderError, e:
-                print _('Error accessing URL: %s') % configfile
-                sys.exit(1)
-        else:
-            if os.access(configfile, os.R_OK):
-                self.cfg.read(configfile)
-            else:
-                print _('Error accessing File: %s') % configfile
-                sys.exit(1)
+        configh = confpp(configfile)
+        try:
+            self.cfg.readfp(configh)
+        except ConfigParser.MissingSectionHeaderError, e:
+            print _('Error accessing config file: %s') % configfile
+            sys.exit(1)
+        
         self.servers = []
         self.servername = {}
         self.serverurl = {}
@@ -308,3 +302,184 @@ class yumconf:
         (string, count) = yum9_reg.subn(self.yumvar[9], string)
         
         return string
+
+
+# Issues/Concerns:
+#   * There are a couple of places that call sys.exit.. it may
+#     be preferable to throw a exception instead and let yumconf
+#     handle the exit
+class confpp:
+    """
+    ConfigParser Include Pre-Processor
+    
+        File-like Object capable of pre-processing include= lines for
+        a ConfigParser. 
+        
+        The readline function expands lines matching include=(url)
+        into lines from the url specified. Includes may occur in
+        included files as well. 
+        
+        Suggested Usage:
+            cfg = ConfigParser.ConfigParser()
+            fileobj = confpp( fileorurl )
+            cfg.readfp(fileobj)
+    """
+    
+    
+    def __init__(self, configfile):
+        # set some file-like object attributes for ConfigParser
+        # these just make confpp look more like a real file object.
+        self.mode = 'r' 
+        
+        # establish whether to use urlgrabber or urllib
+        # we want to use urlgrabber if it supports urlopen
+        if hasattr(urlgrabber,'urlopen'):   self._urlresolver = urlgrabber
+        else: self._urlresolver = urllib
+        
+        # first make configfile a url even if it points to 
+        # a local file
+        scheme = urlparse.urlparse(configfile)[0]
+        if scheme == '':
+            url = 'file://' + configfile
+        else:
+            url = configfile
+        
+        # these are used to maintain the include stack and check
+        # for recursive/duplicate includes
+        self._incstack = []
+        self._alreadyincluded = []
+        
+        # _pushfile will return None if he couldn't open the file
+        fo = self._pushfile( url )
+        if fo is None: 
+            print _('Error accessing file: %s') % url
+            sys.exit(1)
+        
+    
+    def readline( self, size=0 ):
+        """
+        Implementation of File-Like Object readline function. This should be
+        the only function called by ConfigParser according to the python docs.
+        We maintain a stack of real FLOs and delegate readline calls to the 
+        FLO on top of the stack. When EOF occurs on the topmost FLO, it is 
+        popped off the stack and the next FLO takes over. include= lines 
+        found anywhere cause a new FLO to be opened and pushed onto the top 
+        of the stack. Finally, we return EOF when the bottom-most (configfile
+        arg to __init__) FLO returns EOF.
+        
+        Very Technical Pseudo Code:
+        
+        def confpp.readline() [this is called by ConfigParser]
+            open configfile, push on stack
+            while stack has some stuff on it
+                line = readline from file on top of stack
+                pop and continue if line is EOF
+                if line starts with 'include=' then
+                    error if file is recursive or duplicate
+                    otherwise open file, push on stack
+                    continue
+                else
+                    return line
+            
+            return EOF
+        """
+        
+        # set line to EOF initially. 
+        line=''
+        while len(self._incstack) > 0:
+            # peek at the file like object on top of the stack
+            fo = self._incstack[-1]
+            line = fo.readline()
+            if len(line) > 0:
+                m = re.match( r'\s*include\s*=\s*(?P<url>.*)', line )
+                if m:
+                    url = m.group('url')
+                    if len(url) == 0:
+                        print _('Error parsing config %s: include must specify '
+                                ' file to include.') % (self.name)
+                        sys.exit(1)
+                    else:
+                        # whooohoo a valid include line.. push it on the stack
+                        fo = self._pushfile( url )
+                else:
+                    # line didn't match include=, just return it as is
+                    # for the ConfigParser
+                    break
+            else:
+                # the current file returned EOF, pop it off the stack.
+                self._popfile()
+        
+        # at this point we have a line from the topmost file on the stack
+        # or EOF if the stack is empty
+        return line
+    
+    
+    def _absurl( self, url ):
+        """
+        Returns an absolute url for the (possibly) relative
+        url specified. The base url used to resolve the
+        missing bits of url is the url of the file currently
+        being included (i.e. the top of the stack).
+        """
+        
+        if len(self._incstack) == 0:
+            # it's the initial config file. No base url to resolve against.
+            return url
+        else:
+            return urlparse.urljoin( self.geturl(), url )
+    
+    
+    def _pushfile( self, url ):
+        """
+        Opens the url specified, pushes it on the stack, and 
+        returns a file like object. Returns None if the url 
+        has previously been included.
+        If the file can not be opened this function exits.
+        """
+        
+        # absolutize this url using the including files url
+        # as a base url.
+        absurl = self._absurl(url)
+        # check if this has previously been included.
+        if self._urlalreadyincluded(absurl):
+            print _('Warning: Config file %s tried to include %s' 
+                    ' but it has already been included (possible recursion).' 
+                    ' Skipping..') % (self.name,absurl)
+            return None
+        
+        fo = self._urlresolver.urlopen(absurl)
+        if not fo is None:
+            print _('Including %s in config') % absurl
+            self.name = absurl
+            self._incstack.append( fo )
+            self._alreadyincluded.append(absurl)
+        else:
+            print _('Error accessing file: Config file %s tried ' 
+                    'to include %s') % (self.name, absurl)
+            sys.exit(1)
+        return fo
+    
+    
+    def _popfile( self ):
+        """
+        Pop a file off the stack signaling completion of including that file.
+        """
+        fo = self._incstack.pop()
+        fo.close()
+        if len(self._incstack) > 0:
+            self.name = self._incstack[-1].geturl()
+        else:
+            self.name = None
+    
+    
+    def _urlalreadyincluded( self, url ):
+        """
+        Checks if the url has already been included at all.. this 
+        does not necessarily have to be recursive
+        """
+        for eurl in self._alreadyincluded:
+            if eurl == url: return 1
+        return 0
+    
+    
+    def geturl(self): return self.name
