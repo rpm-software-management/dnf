f70c64ecc77672bbc878ae0d4f9c6967ccd52529
Seth Vidal <skvidal@linux.duke.edu>
2004-11-24 16:16:20 +0000

    try to catch keyboard interrupt earlier

--- bin/yum.py
+++ bin/yum.py
@@ -2,11 +2,10 @@
 
 import sys
 sys.path.insert(0, '/usr/share/yum-cli')
-
-import yummain
 try:
+    import yummain
     yummain.main(sys.argv[1:])
 except KeyboardInterrupt, e:
     print >> sys.stderr, "\n\nExiting on user cancel."
     sys.exit(1)
-                                
+
