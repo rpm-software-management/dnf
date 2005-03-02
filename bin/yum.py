4894d578a4db94cc63c7d5d46854050f0f603c57
Seth Vidal <skvidal@linux.duke.edu>
2005-03-02 08:31:17 +0000

    update to yum.py from Terrel Shumway - makes nicer output message if the yum module doesn't exist.

--- bin/yum.py
+++ bin/yum.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
-
 import sys
+try:
+   import yum
+except ImportError:
+   print >> sys.stderr, "The yum libraries do not seem to be available\
+on your system for this version of python ", sys.version
+   print >> sys.stderr, "Please make sure the package you used to install\
+yum was built for your install of python."
+   sys.exit(1)
+
 sys.path.insert(0, '/usr/share/yum-cli')
 try:
     import yummain
@@ -8,4 +16,3 @@ try:
 except KeyboardInterrupt, e:
     print >> sys.stderr, "\n\nExiting on user cancel."
     sys.exit(1)
-
