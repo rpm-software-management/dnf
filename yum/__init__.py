72f228d03a9784996c71befd5f0c56c6a2eada20
Seth Vidal <skvidal@linux.duke.edu>
2007-01-21 20:23:30 +0000

    mark as 3.1.0

--- yum/__init__.py
+++ yum/__init__.py
@@ -49,7 +49,7 @@ import yumRepo
 from packages import parsePackages, YumAvailablePackage, YumLocalPackage, YumInstalledPackage
 from constants import *
 
-__version__ = '3.0'
+__version__ = '3.1.0'
 
 class YumBase(depsolve.Depsolve):
     """This is a primary structure and base class. It houses the objects and
