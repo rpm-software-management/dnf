dbb1cd6b95718f2a2b7b9f803a86e748676464de
Seth Vidal <skvidal@linux.duke.edu>
2007-02-07 06:05:34 +0000

    fix yum deplist to work with new matchPackageNames method

--- cli.py
+++ cli.py
@@ -817,8 +817,8 @@ class YumBaseCli(yum.YumBase, output.YumOutput):
         
         for arg in args:
             pkgs = []
-            pogen = self.pkgSack.matchPackageNames(arg)
-            for po in pogen:
+            ematch, match, unmatch = self.pkgSack.matchPackageNames([arg])
+            for po in ematch + match:
                 pkgs.append(po)
                 
             results = self.findDeps(pkgs)
