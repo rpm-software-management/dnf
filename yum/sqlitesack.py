6d75f1b312ae5fd6017391dcee50c76b27c62fc8
Seth Vidal <skvidal@linux.duke.edu>
2007-02-23 07:26:46 +0000

    keep from tripping over an empty list

--- yum/sqlitesack.py
+++ yum/sqlitesack.py
@@ -514,6 +514,7 @@ class YumSqlitePackageSack(yumRepo.YumPackageSack):
         matched = []
         exactmatch = []
         unmatched = list(pkgspecs)
+
         for p in pkgspecs:
             if re.match('[\*\?\[\]]', p):
                 query = PARSE_QUERY % ({ "op": "glob", "q": p })
@@ -528,7 +529,8 @@ class YumSqlitePackageSack(yumRepo.YumPackageSack):
                 for pkg in cur.fetchall():
                     if self.excludes[rep].has_key(pkg['pkgId']):
                         continue
-                    unmatched.remove(p)
+                    if p in unmatched:
+                        unmatched.remove(p)
                     matchres.append(self.pc(rep,self.db2class(pkg,True)))
 
         exactmatch = misc.unique(exactmatch)
