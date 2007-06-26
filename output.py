d54a8c75d29a837f3ffd680b5fb3f262a3a88d0c
Jeremy Katz <katzj@redhat.com>
2007-06-26 22:31:17 +0000

    if we get a traceback reading the input, just take it as a no (rh#245736)

--- output.py
+++ output.py
@@ -111,8 +111,11 @@ class YumOutput:
     def userconfirm(self):
         """gets a yes or no from the user, defaults to No"""
 
-        while True:            
-            choice = raw_input('Is this ok [y/N]: ')
+        while True:
+            try:
+                choice = raw_input('Is this ok [y/N]: ')
+            except:
+                choice = []
             choice = choice.lower()
             if len(choice) == 0 or choice[0] in ['y', 'n']:
                 break
