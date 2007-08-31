564fd2751ff55d6f003056e5c67d8061b776a4b5
Seth Vidal <skvidal@fedoraproject.org>
2007-08-31 00:44:21 -0400

    apply patch to fix output msg when the repomd.xml can't be found closes rh bug: 258081

--- yum/yumRepo.py
+++ yum/yumRepo.py
@@ -744,8 +744,8 @@ class YumRepository(Repository, config.RepoConf):
         try:
             self._loadRepoXML(text=self)
         except Errors.RepoError, e:
-            msg = "Cannot retrieve repository metadata (repomd.xml) for repository: %s. " + \
-                  "Please verify its path and try again" % self 
+            msg = ("Cannot retrieve repository metadata (repomd.xml) for repository: %s. " 
+                  "Please verify its path and try again" % self )
             raise Errors.RepoError, (msg)
         return self._repoXML
         
