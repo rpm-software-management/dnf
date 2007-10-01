2cd76ab829325ed423d5ec56ff20d712999a4a35
Florian Festi <ffesti@redhat.com>
2007-10-01 14:51:46 +0200

    Use a more defensive path order

--- test/settestpath.py
+++ test/settestpath.py
@@ -2,6 +2,6 @@ import sys
 
 # Adjust path so we can see the src modules running from branch as well
 # as test dir:
-sys.path.insert(0, './')
-sys.path.insert(0, '../')
 sys.path.insert(0, '../../')
+sys.path.insert(0, '../')
+sys.path.insert(0, './')
