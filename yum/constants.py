a0433a8453c21f2c5ba8a34bda53549cb405df8b
James Bowes <jbowes@redhat.com>
2007-11-15 09:00:06 -0500

    Docstring additions for yum.constants

--- yum/constants.py
+++ yum/constants.py
@@ -12,6 +12,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+"""
+Yum constants. Usually dealing with rpm magic numbers.
+"""
 
 #Constants
 YUM_PID_FILE = '/var/run/yum.pid'
