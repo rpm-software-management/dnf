rpm-software-management/dnf

commit febe35eee130212380e5688bd119f19a032d74fc

Make sure rpm.RPMSENSE_* int constants are used as flags in PRCOs. Added short hand constants to testbase Adjusted the test cases

yum/packages.py
@@ -310,8 +310,8 @@ class RpmBase(object):
                 continue
 
             if f == '=':
-                f = 'EQ'
-            if f != 'EQ' and prcotype == 'provides':
+                f = rpm.RPMSENSE_EQUAL
+            if f != rpm.RPMSENSE_EQUAL and prcotype == 'provides':
                 # isn't this odd, it's not 'EQ' and it is a provides
                 # - it really should be EQ
                 # use the pkgobj's evr for the comparison
@@ -752,7 +752,7 @@ class YumHeaderPackage(YumAvailablePackage):
                 continue
 
             lst = hdr[getattr(rpm, 'RPMTAG_%sFLAGS' % tag)]
-            flag = map(rpmUtils.miscutils.flagToString, lst)
+            # flag = map(rpmUtils.miscutils.flagToString, lst)
             flag = map(misc.share_data, flag)
 
             lst = hdr[getattr(rpm, 'RPMTAG_%sVERSION' % tag)]

rpmUtils/miscutils.py
@@ -129,7 +129,7 @@ def rangeCheck(reqtuple, pkgtuple):
     # nameonly shouldn't ever raise it
     #(reqn, reqf, (reqe, reqv, reqr)) = reqtuple
     (n, a, e, v, r) = pkgtuple
-    return rangeCompare(reqtuple, (n, 'EQ', (e, v, r)))
+    return rangeCompare(reqtuple, (n, rpm.RPMSENSE_EQUAL, (e, v, r)))
 
 def rangeCompare(reqtuple, provtuple):
     """returns true if provtuple satisfies reqtuple"""
@@ -140,6 +140,9 @@ def rangeCompare(reqtuple, provtuple):
 
     if f is None or reqf is None:
         return 1
+
+    if isinstance(f, str) or isinstance(reqf, str):
+        raise ValueError, "%r %r" % (f, reqf)
 
     # and you thought we were done having fun
     # if the requested release is left out then we have
@@ -161,42 +164,33 @@ def rangeCompare(reqtuple, provtuple):
 
     # does not match unless
     if rc >= 1:
-        if reqf in ['GT', 'GE', 4, 12]:
+        if reqf & rpm.RPMSENSE_GREATER:
             return 1
-        if reqf in ['EQ', 8]:
-            if f in ['LE', 10]:
+        if reqf == rpm.RPMSENSE_EQUAL:
+            if f == (rpm.RPMSENSE_LESS|rpm.RPMSENSE_EQUAL):
                 return 1
     if rc == 0:
-        if reqf in ['GT', 4]:
-            if f in ['GT', 'GE', 4, 12]:
+        if reqf == rpm.RPMSENSE_GREATER:
+            if f & rpm.RPMSENSE_GREATER:
                 return 1
-        if reqf in ['GE', 12]:
-            if f in ['GT', 'GE', 'EQ', 'LE', 4, 12, 8, 10]:
+        if reqf == (rpm.RPMSENSE_GREATER|rpm.RPMSENSE_EQUAL):
+            if f != rpm.RPMSENSE_LESS:
                 return 1
-        if reqf in ['EQ', 8]:
-            if f in ['EQ', 'GE', 'LE', 8, 12, 10]:
+        if reqf == rpm.RPMSENSE_EQUAL:
+            if f & rpm.RPMSENSE_EQUAL:
                 return 1
-        if reqf in ['LE', 10]:
-            if f in ['EQ', 'LE', 'LT', 'GE', 8, 10, 2, 12]:
+        if reqf == (rpm.RPMSENSE_LESS|rpm.RPMSENSE_EQUAL):
+            if f != rpm.RPMSENSE_GREATER:
                 return 1
-        if reqf in ['LT', 2]:
-            if f in ['LE', 'LT', 10, 2]:
+        if reqf == rpm.RPMSENSE_LESS:
+            if f & rpm.RPMSENSE_LESS:
                 return 1
     if rc <= -1:
-        if reqf in ['GT', 'GE', 'EQ', 4, 12, 8]:
-            if f in ['GT', 'GE', 4, 12]:
+        if not (reqf & rpm.RPMSENSE_LESS):
+            if f & rpm.RPMSENSE_GREATER:
                 return 1
-        if reqf in ['LE', 'LT', 10, 2]:
+        if reqf & rpm.RPMSENSE_LESS:
             return 1
-#                if rc >= 1:
-#                    if reqf in ['GT', 'GE', 4, 12]:
-#                        return 1
-#                if rc == 0:
-#                    if reqf in ['GE', 'LE', 'EQ', 8, 10, 12]:
-#                        return 1
-#                if rc <= -1:
-#                    if reqf in ['LT', 'LE', 2, 10]:
-#                        return 1
 
     return 0
 

test/depsolvetests.py
@@ -64,7 +64,7 @@ class DepsolveTests(DepsolveTests):
 
     def testInstallSinglePackageRequireVer1NotProvided(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'EQ', (None, '1.3', '2'))
+        po.addRequires('zip', EQ, (None, '1.3', '2'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.0', '2', None, 'i386')
@@ -74,7 +74,7 @@ class DepsolveTests(DepsolveTests):
 
     def testInstallSinglePackageRequireVer1Installed(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'EQ', (None, '1.3', '2'))
+        po.addRequires('zip', EQ, (None, '1.3', '2'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '2', None, 'i386')
@@ -85,7 +85,7 @@ class DepsolveTests(DepsolveTests):
 
     def testInstallSinglePackageRequireVer2NotProvided(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'EQ', (None, '1.3', '4'))
+        po.addRequires('zip', EQ, (None, '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '2', None, 'i386')
@@ -95,7 +95,7 @@ class DepsolveTests(DepsolveTests):
 
     def testInstallSinglePackageRequireVer2Installed(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'EQ', (None, '1.3', '4'))
+        po.addRequires('zip', EQ, (None, '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '4', None, 'i386')
@@ -106,7 +106,7 @@ class DepsolveTests(DepsolveTests):
 
     def testInstallSinglePackageRequireVer3NotProvided(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'GE', ('1', '1.3', '4'))
+        po.addRequires('zip', GE, ('1', '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '4', '0', 'i386')
@@ -116,7 +116,7 @@ class DepsolveTests(DepsolveTests):
 
     def testInstallSinglePackageRequireVer3Installed(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'GE', ('2', '1.3', '4'))
+        po.addRequires('zip', GE, ('2', '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '4', '2', 'i386')
@@ -127,7 +127,7 @@ class DepsolveTests(DepsolveTests):
 
     def testInstallSinglePackageRequireVer4NotProvided(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'LT', ('2', '1.3', '4'))
+        po.addRequires('zip', LT, ('2', '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '4', '2', 'i386')
@@ -137,7 +137,7 @@ class DepsolveTests(DepsolveTests):
 
     def testInstallSinglePackageRequireVer4_1Installed(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'LT', ('2', '1.3', '4'))
+        po.addRequires('zip', LT, ('2', '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.0', '4', '2', 'i386')
@@ -147,7 +147,7 @@ class DepsolveTests(DepsolveTests):
         self.assertResult((po, ipo))
     def testInstallSinglePackageRequireVer4_2Installed(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'LT', ('2', '1.3', '4'))
+        po.addRequires('zip', LT, ('2', '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '3', '2', 'i386')
@@ -157,7 +157,7 @@ class DepsolveTests(DepsolveTests):
         self.assertResult((po, ipo))
     def testInstallSinglePackageRequireVer4_3Installed(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'LT', ('2', '1.3', '4'))
+        po.addRequires('zip', LT, ('2', '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '4', None, 'i386')
@@ -167,7 +167,7 @@ class DepsolveTests(DepsolveTests):
         self.assertResult((po, ipo))
     def testInstallSinglePackageRequireVer4_4Installed(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'LT', ('2', '1.3', '4'))
+        po.addRequires('zip', LT, ('2', '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '4', '1', 'i386')
@@ -177,7 +177,7 @@ class DepsolveTests(DepsolveTests):
         self.assertResult((po, ipo))
     def testInstallSinglePackageRequireVer4_5Installed(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'LT', ('2', '1.3', '4'))
+        po.addRequires('zip', LT, ('2', '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '0.3', '4', '2', 'i386')
@@ -188,8 +188,8 @@ class DepsolveTests(DepsolveTests):
 
     def testInstallSinglePackageRequireXtraBadVer(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'EQ', ('2', '1.3', '4'))
-        po.addRequires('zap', 'EQ', ('2', '1.3', '4'))
+        po.addRequires('zip', EQ, ('2', '1.3', '4'))
+        po.addRequires('zap', EQ, ('2', '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '4', '2', 'i386')
@@ -202,8 +202,8 @@ class DepsolveTests(DepsolveTests):
 
     def testInstallSinglePackageRequireXtra(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'EQ', ('2', '1.3', '4'))
-        po.addRequires('zap', 'EQ', ('4', '2.6', '8'))
+        po.addRequires('zip', EQ, ('2', '1.3', '4'))
+        po.addRequires('zap', EQ, ('4', '2.6', '8'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '4', '2', 'i386')
@@ -217,11 +217,11 @@ class DepsolveTests(DepsolveTests):
         
     def testInstallSinglePackageRequireInstalledRequireXtra(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'EQ', ('2', '1.3', '4'))
+        po.addRequires('zip', EQ, ('2', '1.3', '4'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '4', '2', 'i386')
-        ipo.addRequires('zap', 'EQ', ('4', '2.6', '8'))
+        ipo.addRequires('zap', EQ, ('4', '2.6', '8'))
         self.rpmdb.addPackage(ipo)
         
         xpo = FakePackage('zap', '2.6', '8', '4', 'i386')
@@ -232,33 +232,33 @@ class DepsolveTests(DepsolveTests):
         
     def testInstallSinglePackageRequireUpgradeRequireXtraErr(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'EQ', ('4', '2.6', '8'))
+        po.addRequires('zip', EQ, ('4', '2.6', '8'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '4', '2', 'i386')
-        ipo.addRequires('zap', 'EQ', ('2', '1.3', '3'))
+        ipo.addRequires('zap', EQ, ('2', '1.3', '3'))
         self.rpmdb.addPackage(ipo)
         
         xpo = FakePackage('zip', '2.6', '8', '4', 'i386')
-        xpo.addRequires('zap', 'EQ', ('2', '1.3', '4'))
+        xpo.addRequires('zap', EQ, ('2', '1.3', '4'))
         self.xsack.addPackage(xpo)
         xpo = FakePackage('zap', '1.3', '4', '2', 'i386')
-        xpo.addRequires('zsh', 'EQ', ('2', '4', '8'))
+        xpo.addRequires('zsh', EQ, ('2', '4', '8'))
         self.xsack.addPackage(xpo)
 
         self.assertEquals('err', *self.resolveCode())
 
     def testInstallSinglePackageRequireUpgradeRequireXtraOk(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'EQ', ('4', '2.6', '8'))
+        po.addRequires('zip', EQ, ('4', '2.6', '8'))
         self.tsInfo.addInstall(po)
 
         ipo = FakePackage('zip', '1.3', '4', '2', 'i386')
-        ipo.addRequires('zap', 'EQ', ('2', '1.3', '3'))
+        ipo.addRequires('zap', EQ, ('2', '1.3', '3'))
         self.rpmdb.addPackage(ipo)
         
         xpo = FakePackage('zip', '2.6', '8', '4', 'i386')
-        xpo.addRequires('zap', 'EQ', ('2', '1.3', '4'))
+        xpo.addRequires('zap', EQ, ('2', '1.3', '4'))
         self.xsack.addPackage(xpo)
         xpo2 = FakePackage('zap', '1.3', '4', '2', 'i386')
         self.xsack.addPackage(xpo2)
@@ -268,11 +268,11 @@ class DepsolveTests(DepsolveTests):
         
     def testInstallSinglePackageRequireMultiXtra(self):
         po = FakePackage('zsh', '1', '1', None, 'i386')
-        po.addRequires('zip', 'EQ', ('4', '2.6', '8'))
+        po.addRequires('zip', EQ, ('4', '2.6', '8'))
         self.tsInfo.addInstall(po)
 
         xpo = FakePackage('zip', '2.6', '8', '4', 'i386')
-        xpo.addRequires('zap', 'EQ', ('2', '1.3', '4'))
+        xpo.addRequires('zap', EQ, ('2', '1.3', '4'))
         self.xsack.addPackage(xpo)
         
         xpo2 = FakePackage('zap', '1.3', '4', '2', 'i386')
@@ -346,7 +346,7 @@ class DepsolveTests(DepsolveTests):
 
     def testUpdateForDependency(self):
         po = FakePackage('zsh', '1', '1', '0', 'i386')
-        po.addRequires('zip', 'EQ', ('0', '2', '1'))
+        po.addRequires('zip', EQ, ('0', '2', '1'))
         self.tsInfo.addInstall(po)
 
         installedpo = FakePackage('zip', '1', '1', '0', 'i386')
@@ -360,18 +360,18 @@ class DepsolveTests(DepsolveTests):
 
     def testUpdateSplitPackage(self):
         po = FakePackage('zsh', '1', '1', '0', 'i386')
-        po.addRequires('libzip', 'EQ', ('0', '2', '1'))
+        po.addRequires('libzip', EQ, ('0', '2', '1'))
         self.tsInfo.addInstall(po)
 
         installedpo = FakePackage('zip', '1', '1', '0', 'i386')
-        installedpo.addProvides('libzip', 'EQ', ('0', '1', '1'))
+        installedpo.addProvides('libzip', EQ, ('0', '1', '1'))
         self.rpmdb.addPackage(installedpo)
 
         updatepo = FakePackage('zip', '2', '1', '0', 'i386')
-        updatepo.addRequires('zip-libs', 'EQ', ('0', '2', '1'))
+        updatepo.addRequires('zip-libs', EQ, ('0', '2', '1'))
         self.xsack.addPackage(updatepo)
         updatepo2 = FakePackage('zip-libs', '2', '1', '0', 'i386')
-        updatepo2.addProvides('libzip', 'EQ', ('0', '2', '1'))
+        updatepo2.addProvides('libzip', EQ, ('0', '2', '1'))
         self.xsack.addPackage(updatepo2)
 
         self.assertEquals('ok', *self.resolveCode())
@@ -442,7 +442,7 @@ class DepsolveTests(DepsolveTests):
 
     def testUpdateForConflict(self):
         po = FakePackage('zsh', '1', '1', '0', 'i386')
-        po.addConflicts('zip', 'LE', ('0', '1', '1'))
+        po.addConflicts('zip', LE, ('0', '1', '1'))
         self.tsInfo.addInstall(po)
 
         installedpo = FakePackage('zip', '1', '1', '0', 'i386')
@@ -459,7 +459,7 @@ class DepsolveTests(DepsolveTests):
         self.tsInfo.addInstall(po)
 
         installedpo = FakePackage('zip', '1', '1', '0', 'i386')
-        installedpo.addConflicts('zsh', 'LE', ('0', '1', '1'))
+        installedpo.addConflicts('zsh', LE, ('0', '1', '1'))
         self.rpmdb.addPackage(installedpo)
 
         updatepo = FakePackage('zip', '2', '1', '0', 'i386')
@@ -470,11 +470,11 @@ class DepsolveTests(DepsolveTests):
 
     def testUpdateForConflictProvide(self):
         po = FakePackage('zsh', '1', '1', '0', 'i386')
-        po.addConflicts('zippy', 'LE', ('0', '1', '1'))
+        po.addConflicts('zippy', LE, ('0', '1', '1'))
         self.tsInfo.addInstall(po)
 
         installedpo = FakePackage('zip', '1', '1', '0', 'i386')
-        installedpo.addProvides('zippy', 'EQ', ('0', '1', '1'))
+        installedpo.addProvides('zippy', EQ, ('0', '1', '1'))
         self.rpmdb.addPackage(installedpo)
 
         updatepo = FakePackage('zip', '2', '1', '0', 'i386')
@@ -485,17 +485,17 @@ class DepsolveTests(DepsolveTests):
 
     def testUpdateForConflictProvide2(self):
         po = FakePackage('zsh', '1', '1', '0', 'i386')
-        po.addProvides('zippy', 'EQ', ('0', '2', '1'))
+        po.addProvides('zippy', EQ, ('0', '2', '1'))
         self.tsInfo.addInstall(po)
 
         installedpo = FakePackage('zip', '1', '1', '0', 'i386')
-        installedpo.addConflicts('zippy', 'GT', ('0', '1', '1'))
-        installedpo.addConflicts('zippy', 'LT', ('0', '1', '1'))
+        installedpo.addConflicts('zippy', GT, ('0', '1', '1'))
+        installedpo.addConflicts('zippy', LT, ('0', '1', '1'))
         self.rpmdb.addPackage(installedpo)
 
         updatepo = FakePackage('zip', '2', '1', '0', 'i386')
-        updatepo.addConflicts('zippy', 'GT', ('0', '2', '1'))
-        updatepo.addConflicts('zippy', 'LT', ('0', '2', '1'))
+        updatepo.addConflicts('zippy', GT, ('0', '2', '1'))
+        updatepo.addConflicts('zippy', LT, ('0', '2', '1'))
         self.xsack.addPackage(updatepo)
 
         self.assertEquals('ok', *self.resolveCode())
@@ -785,13 +785,13 @@ class DepsolveTests(DepsolveTests):
 
     def testMultiPkgVersions1(self):
         ipo1 = FakePackage('abcd', arch='noarch')
-        ipo1.addRequires('Foo', 'EQ', ('0', '1', '1'))
+        ipo1.addRequires('Foo', EQ, ('0', '1', '1'))
         self.rpmdb.addPackage(ipo1)
         ipo2 = FakePackage('Foo', arch='noarch')
         self.rpmdb.addPackage(ipo2)
         
         xpo = FakePackage('abcd', version='2', arch='noarch')
-        xpo.addRequires('Foo', 'GE', ('0', '2', '1'))
+        xpo.addRequires('Foo', GE, ('0', '2', '1'))
         self.tsInfo.addUpdate(xpo, oldpo=ipo1)
 
         po1 = FakePackage('Foo', arch='noarch')
@@ -806,13 +806,13 @@ class DepsolveTests(DepsolveTests):
 
     def testMultiPkgVersions2(self):
         ipo1 = FakePackage('abcd', arch='i586')
-        ipo1.addRequires('Foo', 'EQ', ('0', '1', '1'))
+        ipo1.addRequires('Foo', EQ, ('0', '1', '1'))
         self.rpmdb.addPackage(ipo1)
         ipo2 = FakePackage('Foo', arch='i586')
         self.rpmdb.addPackage(ipo2)
         
         xpo = FakePackage('abcd', version='2', arch='i586')
-        xpo.addRequires('Foo', 'GE', ('0', '2', '1'))
+        xpo.addRequires('Foo', GE, ('0', '2', '1'))
         self.tsInfo.addUpdate(xpo, oldpo=ipo1)
 
         po1 = FakePackage('Foo', arch='i586')
@@ -827,13 +827,13 @@ class DepsolveTests(DepsolveTests):
 
     def testMultiPkgVersions3(self):
         ipo1 = FakePackage('abcd', arch='i586')
-        ipo1.addRequires('Foo', 'EQ', ('0', '1', '1'))
+        ipo1.addRequires('Foo', EQ, ('0', '1', '1'))
         self.rpmdb.addPackage(ipo1)
         ipo2 = FakePackage('Foo', arch='i586')
         self.rpmdb.addPackage(ipo2)
         
         xpo = FakePackage('abcd', version='2', arch='i586')
-        xpo.addRequires('Foo', 'GE', ('0', '2', '1'))
+        xpo.addRequires('Foo', GE, ('0', '2', '1'))
         self.tsInfo.addUpdate(xpo, oldpo=ipo1)
 
         po1 = FakePackage('Foo', arch='i586')
@@ -851,13 +851,13 @@ class DepsolveTests(DepsolveTests):
 
     def testMultiPkgVersions4(self):
         ipo1 = FakePackage('abcd', arch='i386')
-        ipo1.addRequires('Foo', 'EQ', ('0', '1', '1'))
+        ipo1.addRequires('Foo', EQ, ('0', '1', '1'))
         self.rpmdb.addPackage(ipo1)
         ipo2 = FakePackage('Foo', arch='i386')
         self.rpmdb.addPackage(ipo2)
         
         xpo = FakePackage('abcd', version='2', arch='i386')
-        xpo.addRequires('Foo', 'GE', ('0', '2', '1'))
+        xpo.addRequires('Foo', GE, ('0', '2', '1'))
         self.tsInfo.addUpdate(xpo, oldpo=ipo1)
 
         po1 = FakePackage('Foo', arch='i386')
@@ -880,12 +880,12 @@ class DepsolveTests(DepsolveTests):
         self.tsInfo.addInstall(xpo)
 
         po1 = FakePackage('unison213', version='2.13.16', release='9')
-        po1.addProvides('unison', 'EQ', ('0', '2.13.16', '9'))
-        po1.addObsoletes('unison', 'LT', ('0', '2.27.57', '3'))
+        po1.addProvides('unison', EQ, ('0', '2.13.16', '9'))
+        po1.addObsoletes('unison', LT, ('0', '2.27.57', '3'))
         self.xsack.addPackage(po1)
         po2 = FakePackage('unison227', version='2.27.57', release='7')
-        po2.addProvides('unison', 'EQ', ('0', '2.27.57', '7'))
-        po2.addObsoletes('unison', 'LT', ('0', '2.27.57', '3'))
+        po2.addProvides('unison', EQ, ('0', '2.27.57', '7'))
+        po2.addObsoletes('unison', LT, ('0', '2.27.57', '3'))
         self.xsack.addPackage(po2)
 
         self.assertEquals('ok', *self.resolveCode())
@@ -897,11 +897,11 @@ class DepsolveTests(DepsolveTests):
         self.tsInfo.addInstall(xpo)
 
         po1 = FakePackage('unison213', version='2.13.16', release='9')
-        po1.addProvides('unison', 'EQ', ('0', '2.13.16', '9'))
-        po1.addObsoletes('unison', 'LT', ('0', '2.27.57', '3'))
+        po1.addProvides('unison', EQ, ('0', '2.13.16', '9'))
+        po1.addObsoletes('unison', LT, ('0', '2.27.57', '3'))
         po2 = FakePackage('unison227', version='2.27.57', release='7')
-        po2.addProvides('unison', 'EQ', ('0', '2.27.57', '7'))
-        po2.addObsoletes('unison', 'LT', ('0', '2.27.57', '3'))
+        po2.addProvides('unison', EQ, ('0', '2.27.57', '7'))
+        po2.addObsoletes('unison', LT, ('0', '2.27.57', '3'))
         self.xsack.addPackage(po2)
         self.xsack.addPackage(po1)
 

test/operationstests.py
@@ -9,7 +9,7 @@ class ComplicatedTests(OperationsTests):
         simpleobsoletestests.SimpleObsoletesTests.buildPkgs(pkgs)
         # conflicts
         pkgs.conflicts = FakePackage('super-zippy', '0.3', '1', '0', 'i386')
-        pkgs.conflicts.addConflicts('zsh', 'EQ', ('0', '1', '1'))
+        pkgs.conflicts.addConflicts('zsh', EQ, ('0', '1', '1'))
 
     def testObsoleteForConflict(self):
         p = self.pkgs
@@ -31,11 +31,11 @@ class CombinedUpdateObsoletesTest(OperationsTests):
         pkgs.kdevel_2 = FakePackage('k-devel', '3.5', '2')
         pkgs.kdevel_2.addRequires('k')
         pkgs.klibs_2_i386 = FakePackage('klibs', '3.5', '2', arch='i386')
-        pkgs.klibs_2_i386.addObsoletes('klibs', 'LT', (None, '3.5', '2'))
-        pkgs.klibs_2_i386.addObsoletes('k', 'LT', (None, '3.5', '2'))
+        pkgs.klibs_2_i386.addObsoletes('klibs', LT, (None, '3.5', '2'))
+        pkgs.klibs_2_i386.addObsoletes('k', LT, (None, '3.5', '2'))
         pkgs.klibs_2_x86_64 = FakePackage('klibs', '3.5', '2', arch='x86_64')
-        pkgs.klibs_2_x86_64.addObsoletes('klibs', 'LT', (None, '3.5', '2'))
-        pkgs.klibs_2_x86_64.addObsoletes('k', 'LT', (None, '3.5', '2'))
+        pkgs.klibs_2_x86_64.addObsoletes('klibs', LT, (None, '3.5', '2'))
+        pkgs.klibs_2_x86_64.addObsoletes('k', LT, (None, '3.5', '2'))
 
     def testSelfObsolete(self):
         p = self.pkgs

test/packagetests.py
@@ -3,89 +3,90 @@ import settestpath
 
 from yum import packages
 from rpmUtils import miscutils
+from testbase import * 
 
 class InPrcoRangePackageTests(unittest.TestCase):
 
     def setUp(self):
         self.po = packages.RpmBase()
         self.po.rel = 10
-        self.po.prco['provides'].append(("seth", "EQ", (1, 2, 3)))
-        self.po.prco['requires'].append(("foo", "GE", (4, 5, None)))
+        self.po.prco['provides'].append(("seth", EQ, (1, 2, 3)))
+        self.po.prco['requires'].append(("foo", GE, (4, 5, None)))
 
     def testRequiresEqPass(self):
-        dep = ("foo", "EQ", (4, 5, 0))
+        dep = ("foo", EQ, (4, 5, 0))
         self.assertTrue(self.po.inPrcoRange('requires', dep))
 
     def testRequiresEqFailGt(self):
-        dep = ("foo", "EQ", (4, 4, 0))
+        dep = ("foo", EQ, (4, 4, 0))
         self.assertFalse(self.po.inPrcoRange('requires', dep))
 
     def testProvidesGePass(self):
-        dep = ("seth", "GE", (1, 0, 0))
+        dep = ("seth", GE, (1, 0, 0))
         self.assertTrue(self.po.inPrcoRange('provides', dep)) 
 
     def testProvidesGePassWithEqual(self):
-        dep = ("seth", "GE", (1, 2, 3))
+        dep = ("seth", GE, (1, 2, 3))
         self.assertTrue(self.po.inPrcoRange('provides', dep)) 
 
     def testProvidesGeFailOnEpoch(self):
-        dep = ("seth", "GE", (2, 0, 0))
+        dep = ("seth", GE, (2, 0, 0))
         self.assertFalse(self.po.inPrcoRange('provides', dep)) 
 
     def testProvidesGeFailOnVersion(self):
-        dep = ("seth", "GE", (1, 3, 0))
+        dep = ("seth", GE, (1, 3, 0))
         self.assertFalse(self.po.inPrcoRange('provides', dep))
 
     def testProvidesGeFailOnRelease(self):
-        dep = ("seth", "GE", (1, 2, 4))
+        dep = ("seth", GE, (1, 2, 4))
         self.assertFalse(self.po.inPrcoRange('provides', dep))
 
     def testProvidesGtPass(self):
-        dep = ("seth", "GT", (1, 0, 0))
+        dep = ("seth", GT, (1, 0, 0))
         self.assertTrue(self.po.inPrcoRange('provides', dep))
 
     def testProvidesGtFail(self):
-        dep = ("seth", "GT", (1, 2, 4))
+        dep = ("seth", GT, (1, 2, 4))
         self.assertFalse(self.po.inPrcoRange('provides', dep))
 
     def testProvidesGtFailOnEqual(self):
-        dep = ("seth", "GT", (1, 2, 3))
+        dep = ("seth", GT, (1, 2, 3))
         self.assertFalse(self.po.inPrcoRange('provides', dep))
 
     def testProvidesEqPass(self):
-        dep = ("seth", "EQ", (1, 2, 3))
+        dep = ("seth", EQ, (1, 2, 3))
         self.assertTrue(self.po.inPrcoRange('provides', dep))
 
     def testProvidesEqFailGt(self):
-        dep = ("seth", "EQ", (1, 2, 0))
+        dep = ("seth", EQ, (1, 2, 0))
         self.assertFalse(self.po.inPrcoRange('provides', dep))
 
     def testProvidesEqFailLt(self):
-        dep = ("seth", "EQ", (1, 2, 4))
+        dep = ("seth", EQ, (1, 2, 4))
         self.assertFalse(self.po.inPrcoRange('provides', dep))
 
     def testProvidesLePassEq(self):
-        dep = ("seth", "LE", (1, 2, 3))
+        dep = ("seth", LE, (1, 2, 3))
         self.assertTrue(self.po.inPrcoRange('provides', dep))
 
     def testProvidesLePassGt(self):
-        dep = ("seth", "LE", (1, 5, 2))
+        dep = ("seth", LE, (1, 5, 2))
         self.assertTrue(self.po.inPrcoRange('provides', dep))
 
     def testProvidesLeFail(self):
-        dep = ("seth", "LE", (0, 2, 2))
+        dep = ("seth", LE, (0, 2, 2))
         self.assertFalse(self.po.inPrcoRange('provides', dep))
 
     def testProvidesLtPass(self):
-        dep = ("seth", "LT", (1, 2, 6))
+        dep = ("seth", LT, (1, 2, 6))
         self.assertTrue(self.po.inPrcoRange('provides', dep))
 
     def testProvidesLtFailEq(self):
-        dep = ("seth", "LT", (1, 2, 3))
+        dep = ("seth", LT, (1, 2, 3))
         self.assertFalse(self.po.inPrcoRange('provides', dep))
 
     def testProvidesLtFailGt(self):
-        dep = ("seth", "LT", (1, 0, 2))
+        dep = ("seth", LT, (1, 0, 2))
         self.assertFalse(self.po.inPrcoRange('provides', dep))
 
 
@@ -188,12 +189,13 @@ class RangeCompareTests(unittest.TestCase):
 
     def testRangeCompare(self):
         for requires, provides, result in (
-            (('foo', 'EQ', ('0', '1.4.4', '0')),   ('foo', 'EQ', ('0', '1.4.4', '0')),  1),
-            (('foo', 'EQ', ('0', '1.4.4', '0')),   ('foo', 'EQ', ('0', '1.4.4', None)), 1),
-            (('foo', 'EQ', ('0', '1.4.4', None)),  ('foo', 'EQ', ('0', '1.4.4', '8')),  1),
-            (('foo', 'LT', ('0', '1.5.4', None)),  ('foo', 'EQ', ('0', '1.4.4', '7')),  1),
-            (('foo', 'GE', ('0', '1.4.4', '7.1')), ('foo', 'EQ', ('0', '1.4.4', '7')),  0),
-            (('foo', 'EQ', ('0', '1.4', None)),    ('foo', 'EQ', ('0', '1.4.4', '7')),  0),
-            (('foo', 'GT', ('1', '1.4.4', None)),  ('foo', 'EQ', ('3', '1.2.4', '7')),  1),
+            (('foo', EQ, ('0', '1.4.4', '0')),   ('foo', EQ, ('0', '1.4.4', '0')),  1),
+            (('foo', EQ, ('0', '1.4.4', '0')),   ('foo', EQ, ('0', '1.4.4', None)), 1),
+            (('foo', EQ, ('0', '1.4.4', None)),  ('foo', EQ, ('0', '1.4.4', '8')),  1),
+            (('foo', LT, ('0', '1.5.4', None)),  ('foo', EQ, ('0', '1.4.4', '7')),  1),
+            (('foo', GE, ('0', '1.4.4', '7.1')), ('foo', EQ, ('0', '1.4.4', '7')),  0),
+            (('foo', EQ, ('0', '1.4', None)),    ('foo', EQ, ('0', '1.4.4', '7')),  0),
+            (('foo', GT, ('1', '1.4.4', None)),  ('foo', EQ, ('3', '1.2.4', '7')),  1),
             ):
-            self.assertEquals(miscutils.rangeCompare(requires, provides), result)
+            self.assertEquals(miscutils.rangeCompare(requires, provides), result,
+                              "%r %r: %r" % (requires, provides, result))

test/simpleobsoletestests.py
@@ -226,11 +226,11 @@ class GitMetapackageObsoletesTests(OperationsTests):
         pkgs.metapackage = FakePackage('git', '1.5.4.2', '1', '0', 'x86_64')
         # obsoletes
         pkgs.new_git = FakePackage('git', '1.5.4.4', '1', '0', 'x86_64')
-        pkgs.new_git.addObsoletes('git-core', 'LE', ('0', '1.5.4.3', '1'))
-        pkgs.new_git.addProvides('git-core', 'EQ', ('0', '1.5.4', '1'))
+        pkgs.new_git.addObsoletes('git-core', LE, ('0', '1.5.4.3', '1'))
+        pkgs.new_git.addProvides('git-core', EQ, ('0', '1.5.4', '1'))
 
         pkgs.git_all = FakePackage('git-all', '1.5.4', '1', '0', 'x86_64')
-        pkgs.git_all.addObsoletes('git', 'LE', ('0', '1.5.4.3', '1'))
+        pkgs.git_all.addObsoletes('git', LE, ('0', '1.5.4.3', '1'))
 
 
     def testGitMetapackageOnlyCoreInstalled(self):

commit 3194633958f626f94784ae06b17ed23f37d03451

Fix flag conversion in YumHeaderPackage._populatePrco() broken in febe35eee130212380e5688bd119f19a032d74fc

yum/packages.py
@@ -751,9 +751,7 @@ class YumHeaderPackage(YumAvailablePackage):
             if name is None:
                 continue
 
-            lst = hdr[getattr(rpm, 'RPMTAG_%sFLAGS' % tag)]
-            # flag = map(rpmUtils.miscutils.flagToString, lst)
-            flag = map(misc.share_data, flag)
+            flag = map(misc.share_data, hdr[getattr(rpm, 'RPMTAG_%sFLAGS' % tag)])
 
             lst = hdr[getattr(rpm, 'RPMTAG_%sVERSION' % tag)]
             vers = map(rpmUtils.miscutils.stringToVersion, lst)