61609af516bae2e8eea18ab618df104fce8f16cc
Seth Vidal <skvidal@fedoraproject.org>
2010-02-11 13:05:39 -0500

    don't translate it twice (whoops)

--- yum/__init__.py
+++ yum/__init__.py
@@ -1467,7 +1467,7 @@ class YumBase(depsolve.Depsolve):
                     os.unlink(fo)
 
             if raiseError:
-                msg = _(_('Package does not match intended download. Suggestion: run yum clean metadata'))
+                msg = _('Package does not match intended download. Suggestion: run yum clean metadata')
                 raise URLGrabError(-1, msg)
             else:
                 return False
