83c22bd4521cf8464aed0abf6b9d873ef7b86ac5
James Antill <james@and.org>
2010-08-10 15:09:06 -0400

    Remove old txmbrs when creating a new remove txmbr, reinstall X X, BZ 622913.

--- yum/__init__.py
+++ yum/__init__.py
@@ -3702,6 +3702,14 @@ class YumBase(depsolve.Depsolve):
             if self.conf.protected_packages and po.pkgtup == kern_pkgtup:
                 self.logger.warning(_("Skipping the running kernel: %s") % po)
                 continue
+
+            if self.tsInfo.getMembers(po.pkgtup):
+                # This allows multiple reinstalls and update/downgrade "cancel"
+                for txmbr in self.tsInfo.matchNaevr(po.name):
+                    self.logger.warning(_("Removing %s from the transaction") %
+                                        txmbr)
+                    self.tsInfo.remove(txmbr.pkgtup)
+                # Now start the remove/reinstall
             txmbr = self.tsInfo.addErase(po)
             tx_return.append(txmbr)
         
