72143a2de302829caab36564127b077986d622dc
Panu Matilainen <pmatilai@laiskiainen.org>
2011-02-09 16:58:57 +0200

    Save and restore original transaction flags in wrapper test() method
    ...and here lies the key to the mystery of yum needing to repopulate
    the transaction after a test-run, doh :)

--- rpmUtils/transaction.py
+++ rpmUtils/transaction.py
@@ -110,12 +110,14 @@ class TransactionWrapper:
         """tests the ts we've setup, takes a callback function and a conf dict 
            for flags and what not"""
     
+        origflags = self.getTsFlags()
         self.addTsFlag(rpm.RPMTRANS_FLAG_TEST)
         # FIXME GARBAGE - remove once this is reimplemented elsehwere
         # KEEPING FOR API COMPLIANCE ONLY
         if conf.get('diskspacecheck') == 0:
             self.ts.setProbFilter(rpm.RPMPROB_FILTER_DISKSPACE)
         tserrors = self.ts.run(cb.callback, '')
+        self.ts.setFlags(origflags)
     
         reserrors = []
         if tserrors:
