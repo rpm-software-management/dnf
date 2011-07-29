6ff6cfa3ffe5501ec3794fbca31e0136de58554b
Matthew Miller <mattdm@mattdm.org>
2011-07-28 21:54:45 -0400

    Better comments for the cron scripts.
    No code changes -- everything is behind a "#".

--- yum-cron/yum-update.cron.sh
+++ yum-cron/yum-update.cron.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-# Only run if this flag file is set (by /etc/rc.d/init.d/yum-cron)
+# Only run if this flag is set. The flag is created by the yum-cron init
+# script when the service is started -- this allows one to use chkconfig and
+# the standard "service stop|start" commands to enable or disable yum-cron.
 if [[ ! -f /var/lock/subsys/yum-cron ]]; then
   exit 0
 fi
 
-# Grab config settings
+# Read configuration settings from the sysconfig directory.
 if [[ -f /etc/sysconfig/yum-cron ]]; then
   source /etc/sysconfig/yum-cron
 fi
 
-# Only run on certain days of the week 
+# Only run on certain days of the week, based on the
+# settings in the above-mentioned sysconfig file.
 dow=`date +%w` 
 DAYS_OF_WEEK=${DAYS_OF_WEEK:-0123456} 
 if [[ "${DAYS_OF_WEEK/$dow/}" == "${DAYS_OF_WEEK}" ]]; then 
   exit 0 
 fi 
 
-# Random wait
+# Wait a random number of minutes, again based on
+# the setting in the sysconfig file.
 [[ $RANDOMWAIT -gt 0 ]] && sleep $(( $RANDOM % ($RANDOMWAIT * 60) + 1 ))
 
 # Action!
 exec /usr/sbin/yum-cron update
-
