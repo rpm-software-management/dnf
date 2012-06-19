b4aa5c154aea536e7bfcb6f98243a4ef95eac15a
Ales Kozumplik <ales@redhat.com>
2012-06-19 17:37:39 +0200

    packaging: correct FSF address in .py files.

--- dnf/yum/failover.py
+++ dnf/yum/failover.py
@@ -10,7 +10,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # Copyright 2003 Jack Neely, NC State University
 
 # Here we define a base class for failover methods.  The idea here is that each
--- dnf/yum/pgpmsg.py
+++ dnf/yum/pgpmsg.py
@@ -1,18 +1,19 @@
-##Copyright (C) 2003,2005,2009  Jens B. Jorgensen <jbj1@ultraemail.net>
-##
-##This program is free software; you can redistribute it and/or
-##modify it under the terms of the GNU General Public License
-##as published by the Free Software Foundation; either version 2
-##of the License, or (at your option) any later version.
-##
-##This program is distributed in the hope that it will be useful,
-##but WITHOUT ANY WARRANTY; without even the implied warranty of
-##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##GNU General Public License for more details.
-##
-##You should have received a copy of the GNU General Public License
-##along with this program; if not, write to the Free Software
-##Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# Copyright (C) 2003,2005,2009  Jens B. Jorgensen <jbj1@ultraemail.net>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import struct, time, cStringIO, base64, types
 
 #  We use this so that we can work on python-2.4 and python-2.6, and thus.
