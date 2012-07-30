64e766415ad5065cfca6fa34624f37f3e33c8bfb
Ales Kozumplik <ales@redhat.com>
2012-07-30 17:48:40 +0200

    ut: rename the system repo file to reflect the repo's real name.
    suit the hawkey change d1db6dd.

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -21,7 +21,7 @@ import unittest
 
 class Sanity(unittest.TestCase):
     def test_sanity(self):
-        assert(os.access(base.repo("system.repo"), os.R_OK))
+        assert(os.access(base.repo("@System.repo"), os.R_OK))
         sack = base.mock_yum_base().sack
         assert(sack)
         self.assertEqual(sack.nsolvables, base.SYSTEM_NSOLVABLES)
