f7a9474cebbca7de899a3e28a22e3f2041dd2693
Ales Kozumplik <ales@redhat.com>
2013-01-22 13:42:28 +0100

    cosmetic: trailing whitespace in dnf.yum.parser

--- dnf/yum/parser.py
+++ dnf/yum/parser.py
@@ -59,31 +59,31 @@ class ConfigPreProcessor:
     ConfigParser Include Pre-Processor
 
     File-like Object capable of pre-processing include= lines for
-    a ConfigParser. 
+    a ConfigParser.
 
     The readline function expands lines matching include=(url)
     into lines from the url specified. Includes may occur in
-    included files as well. 
+    included files as well.
 
     Suggested Usage::
         cfg = ConfigParser.ConfigParser()
         fileobj = confpp( fileorurl )
         cfg.readfp(fileobj)
     """
-    
-    
+
+
     def __init__(self, configfile, vars=None):
         # put the vars away in a helpful place
         self._vars = vars
 
         # used to track the current ini-section
         self._section = None
-        
+
         # set some file-like object attributes for ConfigParser
         # these just make confpp look more like a real file object.
-        self.mode = 'r' 
-        
-        # first make configfile a url even if it points to 
+        self.mode = 'r'
+
+        # first make configfile a url even if it points to
         # a local file
         scheme = urlparse.urlparse(configfile)[0]
         if scheme == '':
@@ -93,30 +93,30 @@ class ConfigPreProcessor:
             url = 'file://' + configfile
         else:
             url = configfile
-        
+
         # these are used to maintain the include stack and check
         # for recursive/duplicate includes
         self._incstack = []
         self._alreadyincluded = []
-        
+
         # _pushfile will return None if he couldn't open the file
         fo = self._pushfile( url )
-        if fo is None: 
+        if fo is None:
             raise Errors.ConfigError, 'Error accessing file: %s' % url
-        
+
     def readline( self, size=0 ):
         """
         Implementation of File-Like Object readline function. This should be
         the only function called by ConfigParser according to the python docs.
-        We maintain a stack of real FLOs and delegate readline calls to the 
-        FLO on top of the stack. When EOF occurs on the topmost FLO, it is 
-        popped off the stack and the next FLO takes over. include= lines 
-        found anywhere cause a new FLO to be opened and pushed onto the top 
+        We maintain a stack of real FLOs and delegate readline calls to the
+        FLO on top of the stack. When EOF occurs on the topmost FLO, it is
+        popped off the stack and the next FLO takes over. include= lines
+        found anywhere cause a new FLO to be opened and pushed onto the top
         of the stack. Finally, we return EOF when the bottom-most (configfile
         arg to __init__) FLO returns EOF.
-        
+
         Very Technical Pseudo Code::
-        
+
             def confpp.readline() [this is called by ConfigParser]
                 open configfile, push on stack
                 while stack has some stuff on it
@@ -128,11 +128,11 @@ class ConfigPreProcessor:
                         continue
                     else
                         return line
-                
+
                 return EOF
         """
-        
-        # set line to EOF initially. 
+
+        # set line to EOF initially.
         line=''
         while len(self._incstack) > 0:
             # peek at the file like object on top of the stack
@@ -159,7 +159,7 @@ class ConfigPreProcessor:
             else:
                 # the current file returned EOF, pop it off the stack.
                 self._popfile()
-        
+
         # if the section is prefixed by a space then it is breaks iniparser/configparser
         # so fix it
         broken_sec_match = re.match(r'\s+\[(?P<section>.*)\]', line)
@@ -170,8 +170,8 @@ class ConfigPreProcessor:
         if self._vars:
             return varReplace(line, self._vars)
         return line
-    
-    
+
+
     def _absurl( self, url ):
         """
         Returns an absolute url for the (possibly) relative
@@ -179,7 +179,7 @@ class ConfigPreProcessor:
         missing bits of url is the url of the file currently
         being included (i.e. the top of the stack).
         """
-        
+
         if len(self._incstack) == 0:
             # it's the initial config file. No base url to resolve against.
             return url
@@ -188,12 +188,12 @@ class ConfigPreProcessor:
 
     def _pushfile( self, url ):
         """
-        Opens the url specified, pushes it on the stack, and 
-        returns a file like object. Returns None if the url 
+        Opens the url specified, pushes it on the stack, and
+        returns a file like object. Returns None if the url
         has previously been included.
         If the file can not be opened this function exits.
         """
-        
+
         # absolutize this url using the including files url
         # as a base url.
         absurl = self._absurl(url)
@@ -217,8 +217,8 @@ class ConfigPreProcessor:
                   'Error accessing file for config %s' % (absurl)
 
         return fo
-    
-    
+
+
     def _popfile( self ):
         """
         Pop a file off the stack signaling completion of including that file.
@@ -229,8 +229,8 @@ class ConfigPreProcessor:
             self.name = self._incstack[-1].geturl()
         else:
             self.name = None
-    
-    
+
+
     def _isalreadyincluded( self, tuple ):
         """
         Checks if the tuple describes an include that was already done.
@@ -239,6 +239,6 @@ class ConfigPreProcessor:
         for etuple in self._alreadyincluded:
             if etuple == tuple: return 1
         return 0
-    
-    
+
+
     def geturl(self): return self.name
