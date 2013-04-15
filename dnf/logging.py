3847984f4e9904b02ba4f1bc627db64f3b4c9aff
Ales Kozumplik <ales@redhat.com>
2013-04-15 10:51:17 +0200

    implement dnf.logging module
    this provides a more reasonable version of dnf.yum.logginglevels, though
    leaves things to be desired. For instance, the logging can be configured
    so WARNING messages are not shown yet DEBUG messages are. This is allowed
    for CLI compatibility ('-e' and '-d' options).

---
+++ dnf/logging.py
@@ -0,0 +1,104 @@
+# logging.py
+# DNF Logging Subsystem.
+#
+# Copyright (C) 2013  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+#
+
+from __future__ import absolute_import
+import dnf.const
+import dnf.util
+import logging
+import os
+import sys
+
+SUPERCRITICAL = 100 # do not use this for logging
+CRITICAL      = logging.CRITICAL
+ERROR         = logging.ERROR
+WARNING       = logging.WARNING
+INFO          = logging.INFO
+DEBUG         = logging.DEBUG
+SUBDEBUG      = 6
+
+class MaxLevelFilter(object):
+    def __init__(self, max_level):
+        self.max_level = max_level
+
+    def filter(self, record):
+        if record.levelno >= self.max_level:
+            return 0
+        return 1
+
+_VERBOSE_VAL_MAPPING = {
+    0 : SUPERCRITICAL,
+    1 : logging.INFO,
+    2 : logging.INFO, # the default
+    3 : logging.DEBUG,
+    4 : logging.DEBUG,
+    5 : logging.DEBUG,
+    6 : logging.DEBUG, # verbose value
+    }
+
+def _cfg_verbose_val2level(cfg_errval):
+    assert(0 <= cfg_errval <= 10)
+    return _VERBOSE_VAL_MAPPING.get(cfg_errval, SUBDEBUG)
+
+# Both the DNF default and the verbose default are WARNING. Note that ERROR has
+# no specific level.
+_ERR_VAL_MAPPING = {
+    0 : SUPERCRITICAL,
+    1 : logging.CRITICAL
+    }
+
+def _cfg_err_val2level(cfg_errval):
+    assert(0 <= cfg_errval <= 10)
+    return _ERR_VAL_MAPPING.get(cfg_errval, logging.WARNING)
+
+def _create_filehandler(logfile):
+    if not os.path.exists(logfile):
+        dnf.util.touch(logfile)
+        # By default, make logfiles readable by the user (so the reporting ABRT
+        # user can attach root logfiles).
+        os.chmod(logfile, 0644)
+    handler = logging.FileHandler(logfile)
+    formatter = logging.Formatter("%(asctime)s %(message)s", "%b %d %H:%M:%S")
+    handler.setFormatter(formatter)
+    return handler
+
+def setup(verbose_level, error_level, logdir):
+    logger_dnf = logging.getLogger("dnf")
+    logger_dnf.setLevel(SUBDEBUG)
+
+    # setup stdout
+    stdout = logging.StreamHandler(sys.stdout)
+    stdout.setLevel(verbose_level)
+    stdout.addFilter(MaxLevelFilter(logging.WARNING))
+    logger_dnf.addHandler(stdout)
+
+    # setup stderr
+    stderr = logging.StreamHandler(sys.stderr)
+    stderr.setLevel(error_level)
+    logger_dnf.addHandler(stderr)
+
+    # setup file logger
+    logfile = os.path.join(logdir, dnf.const.LOG)
+    handler = _create_filehandler(logfile)
+    logger_dnf.addHandler(handler)
+
+def setup_from_dnf_levels(verbose_level, error_level, logdir):
+    verbose_level_r = _cfg_verbose_val2level(verbose_level)
+    error_level_r = _cfg_err_val2level(error_level)
+    return setup(verbose_level_r, error_level_r, logdir)
---
+++ tests/test_logging.py
@@ -0,0 +1,104 @@
+# Copyright (C) 2013  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+#
+
+from StringIO import StringIO
+import base
+import dnf.logging
+import logging
+import mock
+import collections
+import operator
+import os
+import tempfile
+import unittest
+
+LogfileEntry = collections.namedtuple('LogfileEntry', ('date', 'time', 'message'))
+
+def _split_logfile_entry(entry):
+    record = entry.split(' ')
+    return LogfileEntry(date=' '.join(record[0:2]),
+                        time=record[2],
+                        message=record[3][:-1]) # strip the trailing '\n'
+
+class TestLogging(base.TestCase):
+    def setUp(self):
+        self.logdir = tempfile.mkdtemp(prefix="dnf-logtest-")
+
+    def tearDown(self):
+        dnf.util.rm_rf(self.logdir)
+
+    @staticmethod
+    def _bench(logger):
+        logger.debug("d")
+        logger.info("i")
+        logger.warning("w")
+        logger.error("e")
+
+    def test_level_conversion(self):
+        self.assertRaises(AssertionError, dnf.logging._cfg_verbose_val2level, 11)
+        self.assertEqual(dnf.logging._cfg_verbose_val2level(0),
+                         dnf.logging.SUPERCRITICAL)
+        self.assertEqual(dnf.logging._cfg_verbose_val2level(7),
+                         dnf.logging.SUBDEBUG)
+
+    def test_setup(self):
+        logger = logging.getLogger("dnf")
+        with mock.patch('sys.stdout', new_callable=StringIO) as stdout, \
+                mock.patch('sys.stderr', new_callable=StringIO) as stderr:
+            dnf.logging.setup(logging.INFO, logging.ERROR, self.logdir)
+            self._bench(logger)
+        self.assertEqual("i\n", stdout.getvalue())
+        self.assertEqual("e\n", stderr.getvalue())
+
+    def test_setup_verbose(self):
+        logger = logging.getLogger("dnf")
+        with mock.patch('sys.stdout', new_callable=StringIO) as stdout, \
+                mock.patch('sys.stderr', new_callable=StringIO) as stderr:
+            dnf.logging.setup(logging.DEBUG, logging.WARNING, self.logdir)
+            self._bench(logger)
+        self.assertEqual("d\ni\n", stdout.getvalue())
+        self.assertEqual("w\ne\n", stderr.getvalue())
+
+    @mock.patch('dnf.logging.setup')
+    def test_setup_from_dnf_levels(self, setup_m):
+        dnf.logging.setup_from_dnf_levels(2, 2, self.logdir)
+        self.assertEqual(setup_m.call_args, mock.call(dnf.logging.INFO,
+                                                      dnf.logging.WARNING,
+                                                      self.logdir))
+        dnf.logging.setup_from_dnf_levels(6, 6, self.logdir)
+        self.assertEqual(setup_m.call_args, mock.call(dnf.logging.DEBUG,
+                                                      dnf.logging.WARNING,
+                                                      self.logdir))
+
+    def test_file_logging(self):
+        # log nothing to the console:
+        dnf.logging.setup(dnf.logging.SUPERCRITICAL, dnf.logging.SUPERCRITICAL,
+                          self.logdir)
+        logger = logging.getLogger("dnf")
+        with mock.patch('sys.stdout', new_callable=StringIO) as stdout, \
+                mock.patch('sys.stderr', new_callable=StringIO) as stderr:
+            logger.info("i")
+            logger.critical("c")
+        self.assertEqual(stdout.getvalue(), '')
+        self.assertEqual(stderr.getvalue(), '')
+        # yet the file should contain both the entries:
+        logfile = os.path.join(self.logdir, "dnf.log")
+        self.assertFile(logfile)
+        with open(logfile) as f:
+            msgs =  map(operator.attrgetter("message"),
+                        map(_split_logfile_entry, f.readlines()))
+        self.assertSequenceEqual(msgs, ['i', 'c'])
