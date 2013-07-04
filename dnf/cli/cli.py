rpm-software-management/dnf

commit 4dad74ef3fa1b2810a38ec005dc45bcf0be6245f

Take out the bulk of YumBaseCli.doTransaction() and put in in Base.do_transaction().
The part that can reasonably by used by API clients should reside in Base.

dnf/cli/cli.py
@@ -44,7 +44,6 @@ import dnf.yum.misc
 from dnf.yum.parser import varReplace
 import dnf.yum.plugins
 from dnf.rpmUtils.arch import isMultiLibArch
-from dnf.yum.rpmtrans import RPMTransaction
 import signal
 from dnf.cli import CliError
 import dnf.cli.commands
@@ -201,79 +200,9 @@ class YumBaseCli(dnf.yum.base.Base, output.YumOutput):
         if self.gpgsigcheck(downloadpkgs) != 0:
             return -1
 
-        # save our dsCallback out
-        dscb = self.dsCallback
-        self.dsCallback = None
-        self.transaction.populate_rpm_ts(self.ts)
-
-        rcd_st = time.time()
-        self.logger.info(_('Running Transaction Check'))
-        msgs = self._run_rpm_check()
-        if msgs:
-            rpmlib_only = True
-            for msg in msgs:
-                if msg.startswith('rpmlib('):
-                    continue
-                rpmlib_only = False
-            if rpmlib_only:
-                print(_("ERROR You need to upgrade rpm to handle:"))
-            else:
-                print(_('ERROR with transaction check vs depsolve:'))
-
-            for msg in msgs:
-                print(to_utf8(msg))
-
-            if rpmlib_only:
-                return 1, [_('RPM needs to be upgraded')]
-            return 1, [_RPM_VERIFY, _RPM_REBUILDDB,
-                       _REPORT_TMPLT % self.conf.bugtracker_url]
-
-        self.logger.debug('Transaction Check time: %0.3f' % (time.time() - rcd_st))
-
-        tt_st = time.time()
-        self.logger.info(_('Running Transaction Test'))
-        if not self.conf.diskspacecheck:
-            self.rpm_probfilter.append(rpm.RPMPROB_FILTER_DISKSPACE)
-
-        self.ts.order() # order the transaction
-        self.ts.clean() # release memory not needed beyond this point
-
-        testcb = RPMTransaction(self, test=True)
-        tserrors = self.ts.test(testcb)
-        del testcb
-
-        if len(tserrors) > 0:
-            errstring = _('Transaction Check Error:\n')
-            for descr in tserrors:
-                errstring += '  %s\n' % to_unicode(descr)
-
-            raise dnf.exceptions.Error, errstring + '\n' + \
-                 self.errorSummary(errstring)
-        self.logger.info(_('Transaction Test Succeeded'))
-
-        self.logger.debug('Transaction Test time: %0.3f' % (time.time() - tt_st))
-
-        # unset the sigquit handler
-        signal.signal(signal.SIGQUIT, signal.SIG_DFL)
-        ts_st = time.time()
-
-        # put back our depcheck callback
-        self.dsCallback = dscb
-        # setup our rpm ts callback
-        cb = RPMTransaction(self, display=output.YumCliRPMCallBack(weakref(self)))
-        if self.conf.debuglevel < 2:
-            cb.display.output = False
-
-        self.logger.info(_('Running Transaction'))
-        resultobject = self.runTransaction(cb=cb)
-
-        self.logger.debug('Transaction time: %0.3f' % (time.time() - ts_st))
-        # close things
+        display = output.YumCliRPMCallBack(weakref(self))
+        resultobject = super(YumBaseCli, self).do_transaction(display)
         self.logger.info(self.post_transaction_output())
-
-        # put back the sigquit handler
-        signal.signal(signal.SIGQUIT, sigquit)
-
         return resultobject.return_code
 
     def gpgsigcheck(self, pkgs):

dnf/yum/base.py
@@ -23,7 +23,7 @@ import functools
 import os
 import operator
 import rpm
-
+import signal
 import types
 import errno
 import time
@@ -38,6 +38,7 @@ import config
 from config import ParsingError, ConfigParser
 import dnf.exceptions
 import dnf.logging
+import dnf.yum.rpmtrans
 import rpmsack
 from dnf.rpmUtils.arch import ArchStorage
 import dnf.rpmUtils.transaction
@@ -199,11 +200,12 @@ class Base(object):
     def activate_persistor(self):
         self._persistor = dnf.persistor.Persistor(self.cache_c.cachedir)
 
-    def activate_sack(self):
+    def activate_sack(self, load_system_repo=True):
         """Prepare the Sack and the Goal objects."""
         start = time.time()
         self._sack = sack.build_sack(self)
-        self._sack.load_system_repo(build_cache=True)
+        if load_system_repo:
+            self._sack.load_system_repo(build_cache=True)
         for r in self.repos.iter_enabled():
             self._add_repo_to_sack(r.id)
         self._sack.configure(self.conf.installonlypkgs)
@@ -627,6 +629,82 @@ class Base(object):
             if msg:
                 return (0, [msg])
         return (rescode, restring)
+
+    def do_transaction(self, display=None):
+        # save our dsCallback out
+        dscb = self.dsCallback
+        self.dsCallback = None
+        self.transaction.populate_rpm_ts(self.ts)
+
+        rcd_st = time.time()
+        self.logger.info(_('Running Transaction Check'))
+        msgs = self._run_rpm_check()
+        if msgs:
+            rpmlib_only = True
+            for msg in msgs:
+                if msg.startswith('rpmlib('):
+                    continue
+                rpmlib_only = False
+            if rpmlib_only:
+                print(_("ERROR You need to upgrade rpm to handle:"))
+            else:
+                print(_('ERROR with transaction check vs depsolve:'))
+
+            for msg in msgs:
+                print(to_utf8(msg))
+
+            if rpmlib_only:
+                return 1, [_('RPM needs to be upgraded')]
+            return 1, [_RPM_VERIFY, _RPM_REBUILDDB,
+                       _REPORT_TMPLT % self.conf.bugtracker_url]
+
+        self.logger.debug('Transaction Check time: %0.3f' % (time.time() - rcd_st))
+
+        tt_st = time.time()
+        self.logger.info(_('Running Transaction Test'))
+        if not self.conf.diskspacecheck:
+            self.rpm_probfilter.append(rpm.RPMPROB_FILTER_DISKSPACE)
+
+        self.ts.order() # order the transaction
+        self.ts.clean() # release memory not needed beyond this point
+
+        testcb = dnf.yum.rpmtrans.RPMTransaction(self, test=True)
+        tserrors = self.ts.test(testcb)
+        del testcb
+
+        if len(tserrors) > 0:
+            errstring = _('Transaction Check Error:\n')
+            for descr in tserrors:
+                errstring += '  %s\n' % to_unicode(descr)
+
+            raise dnf.exceptions.Error, errstring + '\n' + \
+                 self.errorSummary(errstring)
+        self.logger.info(_('Transaction Test Succeeded'))
+
+        self.logger.debug('Transaction Test time: %0.3f' % (time.time() - tt_st))
+
+        # unset the sigquit handler
+        sigquit = signal.signal(signal.SIGQUIT, signal.SIG_DFL)
+        ts_st = time.time()
+
+        # put back our depcheck callback
+        self.dsCallback = dscb
+        # setup our rpm ts callback
+        if display is None:
+            cb = dnf.yum.rpmtrans.RPMTransaction(self)
+        else:
+            cb = dnf.yum.rpmtrans.RPMTransaction(self, display=display)
+        if self.conf.debuglevel < 2:
+            cb.display.output = False
+
+        self.logger.info(_('Running Transaction'))
+        resultobject = self.runTransaction(cb=cb)
+
+        self.logger.debug('Transaction time: %0.3f' % (time.time() - ts_st))
+        # put back the sigquit handler
+        signal.signal(signal.SIGQUIT, sigquit)
+
+        return resultobject
 
     def _record_history(self):
         return self.conf.history_record and \

commit 0e1663a3dc9670ef02b2ea61cde2cb7c6ff803e3

fix: errors handling messages in do_transaction(). (RhBug:981310)
Caused by refactoring done in 4dad74e.

dnf/cli/cli.py
@@ -54,12 +54,6 @@ import hawkey
 
 from dnf.yum.i18n import to_unicode, to_utf8, exception2msg, _, P_
 
-_RPM_VERIFY=_("To diagnose the problem, try running: '%s'.") % \
-    'rpm -Va --nofiles --nodigest'
-_RPM_REBUILDDB=_("To fix inconsistent RPMDB, try running: '%s'.") % \
-    'rpm --rebuilddb'
-_REPORT_TMPLT=_("If the above doesn't help please report this error at '%s'.")
-
 def sigquit(signum, frame):
     """SIGQUIT handler for the yum cli.  This function will print an
     error message and exit the program.
@@ -136,7 +130,7 @@ class YumBaseCli(dnf.yum.base.Base, output.YumOutput):
 
         return summary
 
-    def doTransaction(self):
+    def do_transaction(self):
         """Take care of package downloading, checking, user
         confirmation and actually running the transaction.
 
@@ -148,7 +142,7 @@ class YumBaseCli(dnf.yum.base.Base, output.YumOutput):
         if len(self._transaction) == 0:
             msg = _('Trying to run the transaction but nothing to do. Exiting.')
             self.logger.info(msg)
-            return -1
+            return -1, None
 
         lsts = self.listTransaction()
         self.logger.info(lsts)
@@ -181,7 +175,7 @@ class YumBaseCli(dnf.yum.base.Base, output.YumOutput):
         if self._promptWanted():
             if self.conf.assumeno or not self.userconfirm():
                 self.logger.info(_('Exiting on user Command'))
-                return -1
+                return -1, None
 
         self.logger.info(_('Downloading Packages:'))
         problems = self.download_packages(downloadpkgs, callback_total=\
@@ -198,12 +192,13 @@ class YumBaseCli(dnf.yum.base.Base, output.YumOutput):
 
         # Check GPG signatures
         if self.gpgsigcheck(downloadpkgs) != 0:
-            return -1
+            return -1, None
 
         display = output.YumCliRPMCallBack(weakref(self))
-        resultobject = super(YumBaseCli, self).do_transaction(display)
-        self.logger.info(self.post_transaction_output())
-        return resultobject.return_code
+        return_code, resultmsgs = super(YumBaseCli, self).do_transaction(display)
+        if return_code == 0:
+            self.logger.info(self.post_transaction_output())
+        return return_code, resultmsgs
 
     def gpgsigcheck(self, pkgs):
         """Perform GPG signature verification on the given packages,

dnf/cli/main.py
@@ -204,7 +204,7 @@ def _main(base, args):
 
     # Run the transaction
     try:
-        return_code = base.doTransaction()
+        return_code, resultmsgs = base.do_transaction()
     except plugins.PluginYumExit, e:
         return exPluginExit(e)
     except dnf.exceptions.LockError:
@@ -215,11 +215,9 @@ def _main(base, args):
         return exIOError(e)
 
     # rpm ts.check() failed.
-    if type(return_code) == type((0,)) and len(return_code) == 2:
-        (result, resultmsgs) = return_code
+    if resultmsgs:
         for msg in resultmsgs:
             logger.critical("%s", msg)
-        return_code = result
         if base._ts_save_file:
             logger.info(_("Your transaction was saved, rerun it with:\n yum load-transaction %s") % base._ts_save_file)
     elif return_code < 0:

dnf/yum/base.py
@@ -70,6 +70,12 @@ import dnf.util
 import dnf.rpmUtils.connection
 from dnf import const, queries, sack
 
+_RPM_VERIFY=_("To diagnose the problem, try running: '%s'.") % \
+    'rpm -Va --nofiles --nodigest'
+_RPM_REBUILDDB=_("To fix inconsistent RPMDB, try running: '%s'.") % \
+    'rpm --rebuilddb'
+_REPORT_TMPLT=_("If the above doesn't help please report this error at '%s'.")
+
 class Base(object):
     """This is a primary structure and base class. It houses the
     objects and methods needed to perform most things in yum. It is
@@ -662,7 +668,7 @@ class Base(object):
                 print(_('ERROR with transaction check vs depsolve:'))
 
             for msg in msgs:
-                print(to_utf8(msg))
+                print(i18n.to_utf8(msg))
 
             if rpmlib_only:
                 return 1, [_('RPM needs to be upgraded')]
@@ -709,13 +715,13 @@ class Base(object):
             cb.display.output = False
 
         self.logger.info(_('Running Transaction'))
-        resultobject = self.runTransaction(cb=cb)
+        return_code = self.runTransaction(cb=cb)
 
         self.logger.debug('Transaction time: %0.3f' % (time.time() - ts_st))
         # put back the sigquit handler
         signal.signal(signal.SIGQUIT, sigquit)
 
-        return resultobject
+        return return_code, None
 
     def _record_history(self):
         return self.conf.history_record and \
@@ -788,9 +794,7 @@ class Base(object):
             except:
                 pass
 
-        # make resultobject - just a plain yumgenericholder object
-        resultobject = misc.GenericHolder()
-        resultobject.return_code = 0
+        return_code = 0
         if errors is None:
             pass
         elif len(errors) == 0:
@@ -801,7 +805,7 @@ class Base(object):
             if len(filter(lambda el: el.Failed(), self.ts)) > 0:
                 errstring = _('Warning: scriptlet or other non-fatal errors occurred during transaction.')
                 self.logger.debug(errstring)
-                resultobject.return_code = 1
+                return_code = 1
             else:
                 self.logger.critical(_("Transaction couldn't start (no root?)"))
                 raise dnf.exceptions.YumRPMTransError(msg=_("Could not run transaction."),
@@ -836,10 +840,10 @@ class Base(object):
         self.plugins.run('posttrans')
         # sync up what just happened versus what is in the rpmdb
         if not self.ts.isTsFlagSet(rpm.RPMTRANS_FLAG_TEST):
-            self.verify_transaction(resultobject, cb.verify_tsi_package)
-        return resultobject
+            self.verify_transaction(return_code, cb.verify_tsi_package)
+        return return_code
 
-    def verify_transaction(self, resultobject=None, verify_pkg_cb=None):
+    def verify_transaction(self, return_code, verify_pkg_cb=None):
         """Check that the transaction did what was expected, and
         propagate external yumdb information.  Output error messages
         if the transaction did not do what was expected.
@@ -956,12 +960,9 @@ class Base(object):
 
         self.plugins.run('postverifytrans')
         if self._record_history():
-            ret = -1
-            if resultobject is not None:
-                ret = resultobject.return_code
             rpmdbv = rpmdb_sack.rpmdb_version(self.yumdb)
             self.plugins.run('historyend')
-            self.history.end(rpmdbv, ret)
+            self.history.end(rpmdbv, return_code)
         self.logger.debug('VerifyTransaction time: %0.3f' % (time.time() - vt_st))
 
     def verifyPkg(self, fo, po, raiseError):

tests/test_base.py
@@ -118,7 +118,7 @@ class VerifyTransactionTest(unittest.TestCase):
 
         self.yumbase.transaction.add_install(new_pkg, [])
         self.yumbase.transaction.add_erase(removed_pkg)
-        self.yumbase.verify_transaction()
+        self.yumbase.verify_transaction(0)
         # mock is designed so this returns the exact same mock object it did
         # during the method call:
         yumdb_info = self.yumbase.yumdb.get_package(new_pkg)