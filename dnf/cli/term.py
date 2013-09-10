f3706ca6401070862d78329a2940b89bda65f4f4
Zdenek Pavlas <zpavlas@redhat.com>
2013-09-10 09:11:45 +0200

    remove all users of urlgrabber.progress.terminal_width_cached()

---
+++ dnf/cli/term.py
@@ -0,0 +1,31 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import termios, fcntl, struct
+
+def _term_width(fd=1):
+    """ Get the real terminal width """
+    # Code from http://mail.python.org/pipermail/python-list/2000-May/033365.html
+    try:
+        buf = 'abcdefgh'
+        buf = fcntl.ioctl(fd, termios.TIOCGWINSZ, buf)
+        ret = struct.unpack('hhhh', buf)[1]
+        if ret == 0:
+            return 80
+        if ret < 20:
+            return 20
+        return ret
+    except IOError:
+        return 80
+
