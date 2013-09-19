fe0ed7e7fae7ca77c784e601316e5f0d3125e12e
Zdenek Pavlas <zpavlas@redhat.com>
2013-09-19 10:02:27 +0200

    unit tests: dnf.cli.progress

---
+++ tests/test_cli_progress.py
@@ -0,0 +1,102 @@
+# Copyright (C) 2012-2013  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+
+import dnf.cli.progress
+from tests import mock
+import time
+import unittest
+
+class MockStdout:
+    def __init__(self): self.out = []
+    def write(self, s): self.out.append(s)
+    def flush(self): pass
+
+class ProgressTest(unittest.TestCase):
+    def test_single(self):
+        now = 1379406823.9
+        fo = MockStdout()
+        with mock.patch('dnf.cli.progress._term_width', return_value=60), \
+             mock.patch('dnf.cli.progress.time', lambda: now):
+
+            p = dnf.cli.progress.LibrepoCallbackAdaptor(fo)
+            p.begin('dummy-text')
+            for i in range(6):
+                p.librepo_cb(None, 5, i)
+                self.assertEquals(len(fo.out), i + 1) # always update
+                now += 1.0
+            p.end()
+
+        # this is straightforward..
+        self.assertEquals(fo.out, [
+            'dummy-text  0% [          ] ---  B/s |   0  B     --:-- ETA\r',
+            'dummy-text 20% [==        ] 1.0  B/s |   1  B     00:04 ETA\r',
+            'dummy-text 40% [====      ] 1.0  B/s |   2  B     00:03 ETA\r',
+            'dummy-text 60% [======    ] 1.0  B/s |   3  B     00:02 ETA\r',
+            'dummy-text 80% [========  ] 1.0  B/s |   4  B     00:01 ETA\r',
+            'dummy-text                  1.0  B/s |   5  B     00:05    \n'])
+
+    def test_restart(self):
+        now = 1379406823.9
+        fo = MockStdout()
+        with mock.patch('dnf.cli.progress._term_width', return_value=60), \
+             mock.patch('dnf.cli.progress.time', lambda: now):
+
+            p = dnf.cli.progress.LibrepoCallbackAdaptor(fo)
+            p.begin('dummy-text')
+            for i in range(6):
+                p.librepo_cb(None, 2 if i < 3 else 5, i)
+                self.assertEquals(len(fo.out), i + 1) # always update
+                now += 1
+            p.end()
+
+        # when librepo downloads multiple metadata files, it changes the total
+        # size reported by the callback. we should calculate progress with
+        # the current total, and report both "finished" events.
+        self.assertEquals(fo.out, [
+            'dummy-text  0% [          ] ---  B/s |   0  B     --:-- ETA\r',
+            'dummy-text 50% [=====     ] 1.0  B/s |   1  B     00:01 ETA\r',
+            'dummy-text                  1.0  B/s |   2  B     00:02    \n',
+            'dummy-text 60% [======    ] 1.0  B/s |   3  B     00:02 ETA\r',
+            'dummy-text 80% [========  ] 1.0  B/s |   4  B     00:01 ETA\r',
+            'dummy-text                  1.0  B/s |   5  B     00:05    \n'])
+
+    def test_multi(self):
+        now = 1379406823.9
+        fo = MockStdout()
+        with mock.patch('dnf.cli.progress._term_width', return_value=60), \
+             mock.patch('dnf.cli.progress.time', lambda: now):
+
+            p = dnf.cli.progress.MultiFileProgressMeter(fo)
+            p.start(2, 30)
+            for i in range(11):
+                # emit 1 update, or <end> & update
+                n = len(fo.out) + 1 + (i == 10)
+                p('foo', 10.0, float(i))
+                self.assertEquals(len(fo.out), n)
+                now += 0.5
+
+                # on <end>, there should be no active dl
+                n = len(fo.out) + 1
+                p('bar', 20.0, float(i*2))
+                self.assertEquals(len(fo.out), n)
+                now += 0.5
+
+        # check "end" events
+        self.assertEquals([o for o in fo.out if o.endswith('\n')], [
+'(1/2): foo                  1.0  B/s |  10  B     00:10    \n',
+'(2/2): bar                  2.0  B/s |  20  B     00:10    \n'])
+        # verify we estimated a sane rate (should be around 3 B/s)
+        self.assertTrue(2.0 < p.rate < 4.0)
