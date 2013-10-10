e5432eab826bb99e5ec18f8a56adf2565ac24fba
Radek Holy <rholy@redhat.com>
2013-10-10 13:59:32 +0200

    Add iterables splitting function.

--- dnf/util.py
+++ dnf/util.py
@@ -74,6 +74,13 @@ def group_by_filter(fn, iterable):
         return acc
     return reduce(splitter, iterable, ([], []))
 
+def insert_if(item, iterable, condition):
+    """Insert an item into an iterable by a condition."""
+    for original_item in iterable:
+        if condition(original_item):
+            yield item
+        yield original_item
+
 def is_glob_pattern(pattern):
     return set(pattern) & set("*[?")
 
@@ -130,6 +137,29 @@ def rm_rf(path):
         shutil.rmtree(path)
     except OSError:
         pass
+
+def split_by(iterable, condition):
+    """Split an iterable into tuples by a condition.
+
+    Inserts a separator before each item which meets the condition and then
+    cuts the iterable by these separators.
+
+    """
+    separator = object()  # A unique object.
+    # Create a function returning tuple of objects before the separator.
+    next_subsequence = lambda iterator: tuple(iter(iterator.next, separator))
+
+    # Mark each place where the condition is met by the separator.
+    marked = insert_if(separator, iterable, condition)
+
+    # The 1st subsequence may be empty if the 1st item meets the condition.
+    yield next_subsequence(marked)
+
+    while True:
+        subsequence = next_subsequence(marked)
+        if not subsequence:
+            break
+        yield subsequence
 
 def strip_prefix(s, prefix):
     if s.startswith(prefix):
