rpm-software-management/dnf

commit f475a20eb2a0dbefffee863af1bd6645cc2a7354

basic python 3 support added.
UTs passing under both Pythons now, the main program not yet functional in
Py3.

dnf/util.py
@@ -19,6 +19,7 @@
 #
 
 from __future__ import print_function
+from __future__ import absolute_import
 import dnf.const
 import hawkey
 import librepo
@@ -28,7 +29,8 @@ import subprocess
 import tempfile
 import time
 import types
-
+from functools import reduce
+from .pycomp import PycompDict, PY3, basestring
 """DNF Utilities.
 
 Generally these are not a part of the public DNF API.
@@ -43,7 +45,7 @@ def ensure_dir(dname):
         if not os.path.isdir(dname):
             raise IOError("%s is not a directory" % dname)
     else:
-        os.makedirs(dname, mode=0755)
+        os.makedirs(dname, mode=0o755)
 
 def empty(iterable):
     try:
@@ -56,7 +58,7 @@ def first(iterable):
     """Returns the first item from an iterable or None if it has no elements."""
     it = iter(iterable)
     try:
-        return it.next()
+        return next(it)
     except StopIteration:
         return None
 
@@ -76,7 +78,7 @@ def is_glob_pattern(pattern):
     return set(pattern) & set("*[?")
 
 def is_string_type(obj):
-    return type(obj) in types.StringTypes
+    return isinstance(obj, basestring)
 
 def lazyattr(attrname):
     """Decorator to get lazy attribute initialization.
@@ -112,7 +114,7 @@ def reason_name(reason):
         return "dep"
     if reason == hawkey.REASON_USER:
         return "user"
-    raise ValueError, "Unknown reason %d" % reason
+    raise ValueError("Unknown reason %d" % reason)
 
 def rm_rf(path):
     try:
@@ -164,7 +166,7 @@ class tmpdir(object):
     def __exit__(self, exc_type, exc_value, traceback):
         rm_rf(self.path)
 
-class Bunch(dict):
+class Bunch(PycompDict):
     """Dictionary with attribute accessing syntax.
 
     In DNF, prefer using this over dnf.yum.misc.GenericHolder.
@@ -186,7 +188,10 @@ def urlopen(absurl, **opts):
     """Open the specified absolute url, return a file object.
        'opts' argument is not used atm.
     """
-    fo = tempfile.TemporaryFile()
+    if PY3:
+        fo = tempfile.TemporaryFile(mode='w+', encoding='utf-8')
+    else:
+        fo = tempfile.TemporaryFile()
     librepo.download_url(absurl, fo.fileno(), default_handle)
     fo.seek(0)
     return fo

tests/test_util.py
@@ -16,10 +16,14 @@
 #
 
 from __future__ import absolute_import
-from tests import mock
+try:
+    from unittest import mock
+except ImportError:
+    from tests import mock
 from tests import support
 import dnf.util
 import unittest
+from dnf.pycomp import xrange
 
 class Slow(object):
     def __init__(self, val):

commit e589b843979a32097912c1c1caffb9366c5d08b0

Fix tracebacks during downloads of obsoleting transactions. (RhBug:1021087)
Broken by f475a20.

dnf/base.py
@@ -637,14 +637,14 @@ class Base(object):
                 group_fn = functools.partial(operator.contains, all_obsoleted)
                 obs, upgraded = dnf.util.group_by_filter(
                     group_fn, goal.obsoleted_by_package(pkg))
-                for pkg in obs:
-                    self.ds_callback.pkg_added(pkg, 'od')
+                cb = lambda pkg: self.ds_callback.pkg_added(pkg, 'od')
+                dnf.util.mapall(cb, obs)
                 if pkg.name in self.conf.installonlypkgs:
                     ts.add_install(pkg, obs)
                 else:
                     ts.add_upgrade(pkg, upgraded[0], obs)
-                    for pkg in upgraded:
-                        self.ds_callback.pkg_added(pkg, 'ud')
+                    cb = lambda pkg: self.ds_callback.pkg_added(pkg, 'ud')
+                    dnf.util.mapall(cb, upgraded)
                 self.ds_callback.pkg_added(pkg, 'u')
             for pkg in goal.list_erasures():
                 cnt += 1

dnf/util.py
@@ -96,6 +96,15 @@ def lazyattr(attrname):
         return cached_getter
     return get_decorated
 
+def mapall(fn, *seq):
+    """Like functools.map(), but return a list instead of an iterator.
+
+    This means all side effects of fn take place even without iterating the
+    result.
+
+    """
+    return list(map(fn, *seq))
+
 def on_ac_power():
     """Decide whether we are on line power.
 

tests/test_util.py
@@ -117,6 +117,12 @@ class Util(unittest.TestCase):
         self.assertEqual(slow.square2, 169)
         self.assertEqual(slow.computed, 4)
 
+    def test_mapall(self):
+        l = [1, 2, 3]
+        out = dnf.util.mapall(lambda n: 2 * n, l)
+        self.assertIsInstance(out, list)
+        self.assertEqual(out, [2, 4, 6])
+
     def test_strip_prefix(self):
         self.assertIsNone(dnf.util.strip_prefix("razorblade", "blade"))
         self.assertEqual(dnf.util.strip_prefix("razorblade", "razor"), "blade")