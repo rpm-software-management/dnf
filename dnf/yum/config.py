6c27be44227205e8849c17a5ea1fadf83e684883
Ales Kozumplik <ales@redhat.com>
2013-10-29 11:29:11 +0100

    update the installonlies list.
    most notably, installonly(kernel-module) that ensures kernel-modules-extra stays
    installed was missing.

--- dnf/yum/config.py
+++ dnf/yum/config.py
@@ -768,10 +768,7 @@ class YumConf(StartupConf):
     proxy_password = Option()
     username = Option()
     password = Option()
-    installonlypkgs = ListOption(['kernel', 'kernel-bigmem',
-            'kernel-enterprise','kernel-smp', 'kernel-modules', 'kernel-debug',
-            'kernel-unsupported', 'kernel-source', 'kernel-devel', 'kernel-PAE',
-            'kernel-PAE-debug'])
+    installonlypkgs = ListOption(dnf.const.INSTALLONLYPKGS)
     # NOTE: If you set this to 2, then because it keeps the current kernel it
     # means if you ever install an "old" kernel it'll get rid of the newest one
     # so you probably want to use 3 as a minimum ... if you turn it on.
