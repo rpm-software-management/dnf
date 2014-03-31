0367f1c2768291526e8ec78d3aa196f91d659372
Radek Holy <rholy@redhat.com>
2014-03-31 15:50:51 +0200

    Fix Term.MODE setting under Python 3 in case of incapable tty stdout.

--- tests/test_output.py
+++ tests/test_output.py
@@ -232,6 +232,26 @@ class TermTest(unittest.TestCase):
                           u'reverse': tigetstr(u'rev'),
                           u'underline': tigetstr(u'smul')})
 
+    def test_mode_tty_incapable(self):
+        """Test whether modes correct if the stream is an incapable tty.
+
+        It also ensures that all the values are unicode strings.
+
+        """
+        tty = mock.create_autospec(io.IOBase)
+        tty.isatty.return_value = True
+
+        with mock.patch('curses.tigetstr', autospec=True, return_value=None):
+            term = dnf.cli.output.Term(tty)
+
+        self.assertEqual(term.MODE,
+                         {u'blink': u'',
+                          u'bold': u'',
+                          u'dim': u'',
+                          u'normal': u'',
+                          u'reverse': u'',
+                          u'underline': u''})
+
     def test_mode_nontty(self):
         """Test whether all modes are properly set if the stream is not a tty.
 
