f42ca4ab00ad9c740c8e615f1085324a546f22b6
Ales Kozumplik <ales@redhat.com>
2014-04-01 10:01:32 +0200

    Add dnf.cli.demand.DemandSheet.
    This is the core of what I previously called 'signup mechanism'.
    
    Note that the tests are starting to be structured into subdirectories with
    this commit.

---
+++ dnf/cli/demand.py
@@ -0,0 +1,45 @@
+# demand.py
+# Demand sheet and related classes.
+#
+# Copyright (C) 2014  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+#
+
+class _BoolDefault(object):
+    def __init__(self, default):
+        self.default = default
+        self._storing_name = '__%s%x' % (self.__class__.__name__, id(self))
+
+    def __get__(self, obj, objtype=None):
+        objdict = obj.__dict__
+        if self._storing_name in objdict:
+            return objdict[self._storing_name]
+        return self.default
+
+    def __set__(self, obj, val):
+        objdict = obj.__dict__
+        if self._storing_name in objdict:
+            current_val = objdict[self._storing_name]
+            if current_val != val:
+                raise AttributeError('Demand already set.')
+        objdict[self._storing_name] = val
+
+class DemandSheet(object):
+    available_repos = _BoolDefault(False)
+    resolving = _BoolDefault(False)
+    sack_activation = _BoolDefault(False)
+    success_exit_status = 0
+    writing_rpmdb = _BoolDefault(False)
---
+++ tests/cli/test_demand.py
@@ -0,0 +1,45 @@
+# Copyright (C) 2014  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+
+from __future__ import absolute_import
+
+import dnf.cli.demand
+import tests.support
+
+class DemandTest(tests.support.TestCase):
+
+    def test_bool_default(self):
+        demands = dnf.cli.demand.DemandSheet()
+        demands.resolving = True
+        self.assertTrue(demands.resolving)
+        demands.resolving = True
+        self.assertTrue(demands.resolving)
+        with self.assertRaises(AttributeError):
+            demands.resolving = False
+
+    def test_default(self):
+        demands = dnf.cli.demand.DemandSheet()
+        self.assertFalse(demands.resolving)
+        self.assertFalse(demands.sack_activation)
+        self.assertFalse(demands.writing_rpmdb)
+        self.assertEqual(demands.success_exit_status, 0)
+
+    def test_independence(self):
+        d1 = dnf.cli.demand.DemandSheet()
+        d1.resolving = True
+        d2 = dnf.cli.demand.DemandSheet()
+        self.assertTrue(d1.resolving)
+        self.assertFalse(d2.resolving)
