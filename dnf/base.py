7357f58cac1a67b42e68e9510569aa52c0d126a9
Ales Kozumplik <ales@redhat.com>
2014-07-08 13:50:14 +0200

    logging: RPM transaction markers are too loud.

--- dnf/base.py
+++ dnf/base.py
@@ -680,9 +680,9 @@ class Base(object):
                 except:
                     onice = 0
 
-        self.logger.debug('runTransaction: rpm transaction start.')
+        self.logger.log(dnf.logging.SUBDEBUG, 'RPM transaction start.')
         errors = self.ts.run(cb.callback, '')
-        self.logger.debug('runTransaction: rpm transaction over.')
+        self.logger.log(dnf.logging.SUBDEBUG, 'RPM transaction over.')
         # ts.run() exit codes are, hmm, "creative": None means all ok, empty
         # list means some errors happened in the transaction and non-empty
         # list that there were errors preventing the ts from starting...
