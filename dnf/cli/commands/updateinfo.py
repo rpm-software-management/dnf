a6c60fb98973391688c4f977c7305bc0252de503
Radek Holy <rholy@redhat.com>
2014-07-18 11:41:08 +0200

    Add "updateinfo list" command.

--- dnf/cli/commands/updateinfo.py
+++ dnf/cli/commands/updateinfo.py
@@ -23,14 +23,20 @@
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
+from collections import OrderedDict
 from dnf.cli import commands
 from dnf.i18n import _
 from dnf.pycomp import unicode
+from operator import itemgetter
 
 import collections
 import dnf.exceptions
 import itertools
 import hawkey
+
+def _maxlen(iterable):
+    """Return maximum length of items in a non-empty iterable."""
+    return max(len(item) for item in iterable)
 
 class UpdateInfoCommand(commands.Command):
     """Implementation of the UpdateInfo command."""
@@ -39,29 +45,58 @@ class UpdateInfoCommand(commands.Command):
     summary = _('Display advisories about packages')
     usage = ''
 
+    def __init__(self, cli):
+        """Initialize the command."""
+        super(UpdateInfoCommand, self).__init__(cli)
+        self._ina2evr_cache = None
+        self.clear_installed_cache()
+
+    def refresh_installed_cache(self):
+        """Fill the cache of installed packages."""
+        self._ina2evr_cache = {(pkg.name, pkg.arch): pkg.evr
+                               for pkg in self.base.sack.query().installed()}
+
+    def clear_installed_cache(self):
+        """Clear the cache of installed packages."""
+        self._ina2evr_cache = None
+
+    def _older_installed(self, apackage):
+        """Test whether an older version of a package is installed."""
+        # Non-cached lookup not implemented. Fill the cache or implement the
+        # functionality via the slow sack query.
+        assert self._ina2evr_cache is not None
+        try:
+            ievr = self._ina2evr_cache[(apackage.name, apackage.arch)]
+        except KeyError:
+            return False
+        return self.base.sack.evr_cmp(ievr, apackage.evr) < 0
+
     def configure(self, args):
         """Do any command-specific configuration based on command arguments."""
         super(UpdateInfoCommand, self).configure(args)
         self.cli.demands.sack_activation = True
 
-    def _advisories(self):
-        """Return available advisories."""
-        return itertools.chain.from_iterable(
-            pkg.get_advisories(hawkey.GT)
-            for pkg in self.base.sack.query().installed())
+    def _apackage_advisories(self):
+        """Return (advisory package, advisory) pairs."""
+        for package in self.base.sack.query().installed():
+            for advisory in package.get_advisories(hawkey.GT):
+                for apackage in advisory.packages:
+                    if self._older_installed(apackage):
+                        yield apackage, advisory
 
     @staticmethod
-    def _summary(advisories):
+    def _summary(apkg_advs):
         """Make the summary of advisories."""
         # Remove duplicate advisory IDs. We assume that the ID is unique within
         # a repository and two advisories with the same IDs in different
         # repositories must have the same type.
-        id2type = {advisory.id: advisory.type for advisory in advisories}
+        id2type = {apkg_adv[1].id: apkg_adv[1].type for apkg_adv in apkg_advs}
         return collections.Counter(id2type.values())
 
-    @staticmethod
-    def _display(typ2cnt, description):
+    @classmethod
+    def _display_summary(cls, apkg_advs, description):
         """Display the summary of advisories."""
+        typ2cnt = cls._summary(apkg_advs)
         if not typ2cnt:
             return
         print(_('Updates Information Summary: ') + description)
@@ -72,17 +107,67 @@ class UpdateInfoCommand(commands.Command):
             (_('Enhancement notice(s)'), typ2cnt[hawkey.ADVISORY_ENHANCEMENT]),
             (_('other notice(s)'), typ2cnt[hawkey.ADVISORY_UNKNOWN])]
         # Convert counts to strings and skip missing types.
-        label2value = collections.OrderedDict(
+        label2value = OrderedDict(
             (label, unicode(count)) for label, count in label_counts if count)
-        width = max(len(string) for string in label2value.values())
+        width = _maxlen(label2value.values())
         for label, value in label2value.items():
             print('    %*s %s' % (width, value, label))
 
+    @staticmethod
+    def _list(apkg_advs):
+        """Make the list of advisories."""
+        # Get (NEVRA, advisory ID, advisory type)
+        apkg2nevra = lambda apkg: apkg.name + '-' + apkg.evr + '.' + apkg.arch
+        nevra_id_types = (
+            (apkg2nevra(apkg), adv.id, adv.type)
+            for apkg, adv in apkg_advs)
+        # Sort and group by NEVRAs.
+        nevra_nits = itertools.groupby(
+            sorted(nevra_id_types, key=itemgetter(0)), key=itemgetter(0))
+        for nevra, nits in nevra_nits:
+            # Remove duplicate IDs. We assume that two advisories with the same
+            # IDs (e.g. from different repositories) must have the same type.
+            yield nevra, {nit[1]: nit[2] for nit in nits}
+
+    @classmethod
+    def _display_list(cls, apkg_advs, description):
+        """Display the list of advisories."""
+
+        type2lbl = {hawkey.ADVISORY_BUGFIX: _('bugfix'),
+                    hawkey.ADVISORY_ENHANCEMENT: _('enhancement'),
+                    hawkey.ADVISORY_SECURITY: _('security'),
+                    hawkey.ADVISORY_UNKNOWN: _('unknown')}
+
+        nevra_id2types = cls._list(apkg_advs)
+        # Sort IDs and convert types to labels.
+        nevra2id2tlbl = OrderedDict(
+            (nevra, OrderedDict(sorted(((id_, type2lbl[typ])
+                                        for id_, typ in id2type.items()),
+                                       key=itemgetter(0))))
+            for nevra, id2type in nevra_id2types)
+        if not nevra2id2tlbl:
+            return
+        # Get all advisory IDs and types as two iterables.
+        ids, tlbls = zip(*itertools.chain.from_iterable(
+            id2tlbl.items() for id2tlbl in nevra2id2tlbl.values()))
+        idw, tlw = _maxlen(ids), _maxlen(tlbls)
+        for nevra, id2tlbl in nevra2id2tlbl.items():
+            for id_, tlbl in id2tlbl.items():
+                print('%-*s %-*s %s' % (idw, id_, tlw, tlbl, nevra))
+
     def run(self, args):
         """Execute the command with arguments."""
+
         super(UpdateInfoCommand, self).run(args)
+        display = self._display_summary
         if args[:1] in (['summary'], []):
             args = args[1:]
+        elif args[:1] == ['list']:
+            display, args = self._display_list, args[1:]
+
         if args not in (['available'], []):
             raise dnf.exceptions.Error('invalid command arguments')
-        self._display(self._summary(self._advisories()), _('available'))
+
+        self.refresh_installed_cache()
+        display(self._apackage_advisories(), _('available'))
+        self.clear_installed_cache()
