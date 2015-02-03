57a26a68b24afe98a01b1ecf0e3ed5c6fc0a5cf9
Radek Holy <rholy@redhat.com>
2015-02-03 16:26:01 +0100

    cosmetic: reorder tests in test_install

--- tests/test_install.py
+++ tests/test_install.py
@@ -26,15 +26,27 @@ class MultilibCommonTest(support.ResultTestCase):
     """Tests common to any multilib_policy."""
 
     def setUp(self):
-        self.base = support.MockBase('main', 'third_party')
+        self.base = support.MockBase('main')
         self.base.conf.multilib_policy = "all"
 
-    def test_install_nonexistent(self):
-        """Test that the exception is raised if no package matches."""
-        cnt = self.base.install("not-available")
-        self.assertEqual(cnt, 0)
-        installed_pkgs = self.base.sack.query().installed().run()
-        self.assertResult(self.base, installed_pkgs)
+    def test_install_arch_glob(self):
+        """Test that the pkg specification can contain an architecture glob."""
+        self.base.install("lotus.*6*")
+        installed, removed = self.installed_removed(self.base)
+        self.assertCountEqual(map(str, installed),
+                              ['lotus-3-16.i686',
+                               'lotus-3-16.x86_64'])
+
+    def test_install_filename_glob(self):
+        """Test that the pkg to be installed can be specified by fname glob."""
+        self.base.install("/*/be/there")
+        (installed, _) = self.installed_removed(self.base)
+        self.assertCountEqual(map(str, installed), ('trampoline-2.1-1.noarch',))
+
+        self.base.reset(goal=True)
+        self.base.install("*/there")
+        (installed, _) = self.installed_removed(self.base)
+        self.assertCountEqual(map(str, installed), ('trampoline-2.1-1.noarch',))
 
     def test_install_name(self):
         """Test that the package to be installed can be specified by name."""
@@ -46,6 +58,64 @@ class MultilibCommonTest(support.ResultTestCase):
         new_set = self.base.sack.query().installed() + expected
         self.assertResult(self.base, new_set)
 
+    def test_install_name_glob(self):
+        """Test that the pkg to be installed can be specified by name glob."""
+        self.base.install("mrkite*")
+        q = self.base.sack.query().available().filter(name="mrkite*")
+        new_set = self.base.sack.query().installed() + q.run()
+        installed, removed = self.installed_removed(self.base)
+        self.assertCountEqual(map(str, installed),
+                              ['mrkite-2-0.x86_64',
+                               'mrkite-k-h-1-1.x86_64',
+                               'trampoline-2.1-1.noarch'])
+
+    def test_install_name_glob_exclude(self):
+        """Test that glob excludes play well with glob installs."""
+        subj_ex = dnf.subject.Subject('*-1')
+        pkgs_ex = subj_ex.get_best_query(self.base.sack)
+        self.base.sack.add_excludes(pkgs_ex)
+
+        self.base.install('mrkite*')
+        (installed, _) = self.installed_removed(self.base)
+        self.assertCountEqual(map(str, installed),
+                              ['mrkite-2-0.x86_64',
+                               'trampoline-2.1-1.noarch'])
+
+    def test_install_nevra(self):
+        """Test that the package to be installed can be specified by NEVRA."""
+        self.base.install("lotus-3-16.i686")
+        lotus, = dnf.subject.Subject('lotus-3-16.i686').get_best_query(self.base.sack)
+        new_set = self.base.sack.query().installed() + [lotus]
+        self.assertResult(self.base, new_set)
+
+    def test_install_provide_version(self):
+        """Test that the pkg to be installed can be spec. by provide ver."""
+        self.base.install('splendid > 2.0')
+        self.assertGreater(self.base._goal.req_length(), 0)
+        trampoline = self.base.sack.query().available().filter(
+            name="trampoline")
+        new_set = self.base.sack.query().installed() + trampoline.run()
+        self.assertResult(self.base, new_set)
+
+    @support.mock.patch('dnf.transaction._', dnf.pycomp.NullTranslations().ugettext)
+    def test_install_srpm(self):
+        """Test that the exception is raised if a source arch is specified."""
+        self.base.install("pepper-20-0.src")
+        re = 'Will not install a source rpm'
+        self.assertRaisesRegexp(dnf.exceptions.Error, re,
+                                self.base.resolve)
+
+    def test_package_install_installed(self):
+        """Test that nothing changes if an installed package matches."""
+        p = self.base.sack.query().available().nevra("librita-1-1.x86_64")[0]
+        self.base.package_install(p)
+        self.base.resolve()
+        self.assertEmpty(self.base._goal.list_reinstalls())
+
+        self.base.package_reinstall(p)
+        self.base.resolve()
+        self.assertLength(self.base._goal.list_reinstalls(), 1)
+
     def test_pkg_install_installable(self):
         """Test that the package to be installed can be a package instance."""
         self.base = support.MockBase('main', 'multilib')
@@ -55,10 +125,14 @@ class MultilibCommonTest(support.ResultTestCase):
         self.base.resolve()
         self.assertEqual(1, len(self.base._goal.list_installs()))
 
-    def test_install_provide(self):
-        """Test that the pkg to be installed can be specified by provide."""
-        self.base.install("henry(the_horse)")
-        self.assertGreater(self.base._goal.req_length(), 0)
+class MultilibAllTest(support.ResultTestCase):
+
+    """Tests for multilib_policy='all'."""
+
+    def setUp(self):
+        self.base = support.MockBase('main', 'third_party')
+        self.installed = self.base.sack.query().installed().run()
+        self.base.conf.multilib_policy = "all"
 
     def test_install_filename(self):
         """Test that the pkg to be installed can be specified by filename."""
@@ -66,12 +140,26 @@ class MultilibCommonTest(support.ResultTestCase):
         inst, _ = self.installed_removed(self.base)
         self.assertCountEqual(map(str, inst), ['lotus-3-16.x86_64'])
 
-    def test_install_nevra(self):
-        """Test that the package to be installed can be specified by NEVRA."""
-        self.base.install("lotus-3-16.i686")
-        lotus, = dnf.subject.Subject('lotus-3-16.i686').get_best_query(self.base.sack)
-        new_set = self.base.sack.query().installed() + [lotus]
-        self.assertResult(self.base, new_set)
+    def test_install_multilib(self):
+        """Test that pkgs for all architectures are installed if available."""
+        cnt = self.base.install("lotus")
+        self.assertEqual(cnt, 2)
+        installed = self.installed_removed(self.base)[0]
+        self.assertLessEqual(
+            set(installed),
+            set(self.base.sack.query().available().filter(name='lotus')))
+
+    def test_install_nonexistent(self):
+        """Test that the exception is raised if no package matches."""
+        cnt = self.base.install("not-available")
+        self.assertEqual(cnt, 0)
+        installed_pkgs = self.base.sack.query().installed().run()
+        self.assertResult(self.base, installed_pkgs)
+
+    def test_install_provide(self):
+        """Test that the pkg to be installed can be specified by provide."""
+        self.base.install("henry(the_horse)")
+        self.assertGreater(self.base._goal.req_length(), 0)
 
     def test_install_reponame(self):
         """Test whether packages are filtered by the reponame."""
@@ -88,42 +176,6 @@ class MultilibCommonTest(support.ResultTestCase):
                 'which matches the pattern but is preferred, otherwise the '
                 'test makes no sense')
 
-    @support.mock.patch('dnf.transaction._', dnf.pycomp.NullTranslations().ugettext)
-    def test_install_srpm(self):
-        """Test that the exception is raised if a source arch is specified."""
-        self.base.install("pepper-20-0.src")
-        re = 'Will not install a source rpm'
-        self.assertRaisesRegexp(dnf.exceptions.Error, re,
-                                self.base.resolve)
-
-class MultilibAllTest(support.ResultTestCase):
-
-    """Tests for multilib_policy='all'."""
-
-    def setUp(self):
-        self.base = support.MockBase("main")
-        self.installed = self.base.sack.query().installed().run()
-        self.base.conf.multilib_policy = "all"
-
-    def test_install_multilib(self):
-        """Test that pkgs for all architectures are installed if available."""
-        cnt = self.base.install("lotus")
-        self.assertEqual(cnt, 2)
-        q = self.base.sack.query().available().filter(name="lotus")
-        new_set = self.installed + q.run()
-        self.assertResult(self.base, new_set)
-
-    def test_package_install_installed(self):
-        """Test that nothing changes if an installed package matches."""
-        p = self.base.sack.query().available().nevra("librita-1-1.x86_64")[0]
-        self.base.package_install(p)
-        self.base.resolve()
-        self.assertEmpty(self.base._goal.list_reinstalls())
-
-        self.base.package_reinstall(p)
-        self.base.resolve()
-        self.assertLength(self.base._goal.list_reinstalls(), 1)
-
 
 class MultilibBestTest(support.ResultTestCase):
 
@@ -134,20 +186,14 @@ class MultilibBestTest(support.ResultTestCase):
         self.installed = self.base.sack.query().installed().run()
         self.assertEqual(self.base.conf.multilib_policy, "best")
 
-    def test_install_nonexistent(self):
-        """Test that the exception is raised if no package matches."""
-        with self.assertRaises(dnf.exceptions.MarkingError) as context:
-            self.base.install('not-available')
-        self.assertEqual(context.exception.pkg_spec, 'not-available')
-        installed_pkgs = self.base.sack.query().installed().run()
-        self.assertResult(self.base, installed_pkgs)
+    def test_install_filename(self):
+        """Test that the pkg to be installed can be specified by filename."""
+        self.base.install("/usr/lib*/liblot*")
+        inst, _ = self.installed_removed(self.base)
+        self.assertCountEqual(map(str, inst), ['lotus-3-16.x86_64'])
 
-    def test_install_unavailable(self):
-        """Test that nothing changes if an unavailable package matches."""
-        cnt = self.base.install("hole")
-        self.assertEqual(cnt, 1)
-        installed_pkgs = self.base.sack.query().installed().run()
-        self.assertResult(self.base, installed_pkgs)
+        self.assertRaises(dnf.exceptions.MarkingError,
+                          self.base.install, "/not/exist/")
 
     def test_install_multilib(self):
         """Test that a pkg for only one architecture are installed."""
@@ -158,6 +204,14 @@ class MultilibBestTest(support.ResultTestCase):
         new_set = self.installed + [new_package]
         self.assertResult(self.base, new_set)
 
+    def test_install_nonexistent(self):
+        """Test that the exception is raised if no package matches."""
+        with self.assertRaises(dnf.exceptions.MarkingError) as context:
+            self.base.install('not-available')
+        self.assertEqual(context.exception.pkg_spec, 'not-available')
+        installed_pkgs = self.base.sack.query().installed().run()
+        self.assertResult(self.base, installed_pkgs)
+
     def test_install_provide(self):
         """Test that the pkg to be installed can be specified by provide."""
         self.base.install("henry(the_horse)")
@@ -166,66 +220,6 @@ class MultilibBestTest(support.ResultTestCase):
             name="trampoline")
         new_set = self.installed + trampoline.run()
         self.assertResult(self.base, new_set)
-
-    def test_install_provide_version(self):
-        """Test that the pkg to be installed can be spec. by provide ver."""
-        self.base.install('splendid > 2.0')
-        self.assertGreater(self.base._goal.req_length(), 0)
-        trampoline = self.base.sack.query().available().filter(
-            name="trampoline")
-        new_set = self.installed + trampoline.run()
-        self.assertResult(self.base, new_set)
-
-    def test_install_filename(self):
-        """Test that the pkg to be installed can be specified by filename."""
-        self.base.install("/usr/lib*/liblot*")
-        inst, _ = self.installed_removed(self.base)
-        self.assertCountEqual(map(str, inst), ['lotus-3-16.x86_64'])
-
-        self.assertRaises(dnf.exceptions.MarkingError,
-                          self.base.install, "/not/exist/")
-
-    def test_install_filename_glob(self):
-        """Test that the pkg to be installed can be specified by fname glob."""
-        self.base.install("/*/be/there")
-        (installed, _) = self.installed_removed(self.base)
-        self.assertCountEqual(map(str, installed), ('trampoline-2.1-1.noarch',))
-
-        self.base.reset(goal=True)
-        self.base.install("*/there")
-        (installed, _) = self.installed_removed(self.base)
-        self.assertCountEqual(map(str, installed), ('trampoline-2.1-1.noarch',))
-
-    def test_install_name_glob(self):
-        """Test that the pkg to be installed can be specified by name glob."""
-        self.base.install("mrkite*")
-        q = self.base.sack.query().available().filter(name="mrkite*")
-        new_set = self.installed + q.run()
-        installed, removed = self.installed_removed(self.base)
-        self.assertCountEqual(map(str, installed),
-                              ['mrkite-2-0.x86_64',
-                               'mrkite-k-h-1-1.x86_64',
-                               'trampoline-2.1-1.noarch'])
-
-    def test_install_arch_glob(self):
-        """Test that the pkg specification can contain an architecture glob."""
-        self.base.install("lotus.*6*")
-        installed, removed = self.installed_removed(self.base)
-        self.assertCountEqual(map(str, installed),
-                              ['lotus-3-17.i686',
-                               'lotus-3-17.x86_64'])
-
-    def test_install_name_glob_exclude(self):
-        """Test that glob excludes play well with glob installs."""
-        subj_ex = dnf.subject.Subject('*-1')
-        pkgs_ex = subj_ex.get_best_query(self.base.sack)
-        self.base.sack.add_excludes(pkgs_ex)
-
-        self.base.install('mrkite*')
-        (installed, _) = self.installed_removed(self.base)
-        self.assertCountEqual(map(str, installed),
-                              ['mrkite-2-0.x86_64',
-                               'trampoline-2.1-1.noarch'])
 
     def test_install_reponame(self):
         """Test whether packages are filtered by the reponame."""
@@ -240,3 +234,10 @@ class MultilibBestTest(support.ResultTestCase):
                ('the base must contain packages a package in another repo '
                 'which matches the pattern but is preferred, otherwise the '
                 'test makes no sense')
+
+    def test_install_unavailable(self):
+        """Test that nothing changes if an unavailable package matches."""
+        cnt = self.base.install("hole")
+        self.assertEqual(cnt, 1)
+        installed_pkgs = self.base.sack.query().installed().run()
+        self.assertResult(self.base, installed_pkgs)
