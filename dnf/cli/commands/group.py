rpm-software-management/dnf

commit 02c3cc3b4514364a4adbb12ab0d64607e2acd44a

dnf group list now checks every package and prints out only invalid ones

dnf/cli/commands/group.py
@@ -163,13 +163,18 @@ class GroupCommand(commands.Command):
         if not userlist:
             userlist = None # Match everything...
 
+        errs = False
+        for group in userlist:
+            in_group = len(self.base.comps.groups_by_pattern(group)) > 0
+            in_environment = len(self.base.comps.environments_by_pattern(group)) > 0
+            if not in_group and not in_environment:
+                logger.error(_('Warning: No groups match:\n   %s'), group)
+                errs = True
+        if errs:
+            return 0, []
+
         env_inst, env_avail = self._environment_lists(userlist)
         installed, available = self._group_lists(uservisible, userlist)
-
-        if not any([env_inst, env_avail, installed, available]):
-            logger.error(_('Warning: No groups match: %s'),
-                              ", ".join(userlist))
-            return 0, []
 
         def _out_grp(sect, group):
             if not done:

commit a0dd8ba3c61e00b19d74158e1fa1410a19ba56de

Fix regression in group list command introduced by 02c3cc3

dnf/cli/commands/group.py
@@ -164,14 +164,15 @@ class GroupCommand(commands.Command):
             userlist = None # Match everything...
 
         errs = False
-        for group in userlist:
-            in_group = len(self.base.comps.groups_by_pattern(group)) > 0
-            in_environment = len(self.base.comps.environments_by_pattern(group)) > 0
-            if not in_group and not in_environment:
-                logger.error(_('Warning: No groups match:\n   %s'), group)
-                errs = True
-        if errs:
-            return 0, []
+        if userlist is not None:
+            for group in userlist:
+                in_group = len(self.base.comps.groups_by_pattern(group)) > 0
+                in_environment = len(self.base.comps.environments_by_pattern(group)) > 0
+                if not in_group and not in_environment:
+                    logger.error(_('Warning: No groups match:\n   %s'), group)
+                    errs = True
+            if errs:
+                return 0, []
 
         env_inst, env_avail = self._environment_lists(userlist)
         installed, available = self._group_lists(uservisible, userlist)