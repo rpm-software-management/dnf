b604e9b4434c8ce2070aad26684c064e97fc85c1
Jaroslav Mracek <jmracek@redhat.com>
2016-07-01 13:11:36 +0200

    Make private property from 'sync_strategy' in repo.Repo
    To make more distinguishable API and private functions the property
    sync_strategy was renamed to _sync_strategy.
    
    Closes: #532
    Approved by: ignatenkobrain

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -163,7 +163,7 @@ class CliTest(TestCase):
         self.assertTrue(self.base.repos['comb'].enabled)
         self.assertFalse(self.base.repos["comb"].gpgcheck)
         self.assertFalse(self.base.repos["comb"].repo_gpgcheck)
-        self.assertEqual(self.base.repos["comb"].sync_strategy,
+        self.assertEqual(self.base.repos["comb"]._sync_strategy,
                          dnf.repo.SYNC_ONLY_CACHE)
 
     def test_configure_repos_expired(self, _):
@@ -183,7 +183,7 @@ class CliTest(TestCase):
         self.cli.demands.fresh_metadata = False
         self.cli.demands.cacheonly = True
         self.cli._process_demands()
-        self.assertEqual(self.base.repos['one'].sync_strategy,
+        self.assertEqual(self.base.repos['one']._sync_strategy,
                          dnf.repo.SYNC_ONLY_CACHE)
 
 @mock.patch('dnf.logging.Logging._setup', new=mock.MagicMock)
