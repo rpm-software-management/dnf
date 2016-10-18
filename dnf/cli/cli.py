f740ab4c249309320254be18ffc8a47cc2d79c35
Jaroslav Mracek <jmracek@redhat.com>
2016-10-18 09:23:49 +0200

    Suppress all errors with --quiet option (RhBug:1384289)
    https://bugzilla.redhat.com/show_bug.cgi?id=1384289

--- dnf/cli/cli.py
+++ dnf/cli/cli.py
@@ -825,6 +825,7 @@ class Cli(object):
 
         if opts.quiet:
             opts.debuglevel = 0
+            opts.errorlevel = 0
         if opts.verbose:
             opts.debuglevel = opts.errorlevel = dnf.const.VERBOSE_LEVEL
 
