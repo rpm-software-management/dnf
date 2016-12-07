e2a199247e48aeef15f9e9ab41a7818c9fd1ede1
Jaroslav Mracek <jmracek@redhat.com>
2016-12-07 09:04:32 +0100

    Add support of arguments nevra forms for autoremove command
    It is a feature implemented according to yum-dnf compatibility

--- dnf/cli/commands/autoremove.py
+++ dnf/cli/commands/autoremove.py
@@ -21,26 +21,78 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from dnf.cli import commands
+from dnf.cli.option_parser import OptionParser
 from dnf.i18n import _
+
+import dnf.exceptions
+import hawkey
+import logging
+
+logger = logging.getLogger("dnf")
 
 
 class AutoremoveCommand(commands.Command):
 
-    aliases = ('autoremove',)
+    nevra_forms = {'autoremove-n': hawkey.FORM_NAME,
+                   'autoremove-na': hawkey.FORM_NA,
+                   'autoremove-nevra': hawkey.FORM_NEVRA}
+
+    aliases = ('autoremove',) + tuple(nevra_forms.keys())
     summary = _('remove all unneeded packages that were originally installed '
                 'as dependencies')
 
+    @staticmethod
+    def set_argparser(parser):
+        parser.add_argument('packages', nargs='*', help=_('Package to remove'),
+                            action=OptionParser.ParseSpecGroupFileCallback,
+                            metavar=_('PACKAGE'))
+
     def configure(self):
         demands = self.cli.demands
-        demands.available_repos = True
-        demands.fresh_metadata = False
         demands.resolving = True
         demands.root_user = True
         demands.sack_activation = True
+        self.forms = [self.nevra_forms[command] for command in self.opts.command
+                      if command in list(self.nevra_forms.keys())]
+        if any([self.opts.grp_specs, self.opts.pkg_specs, self.opts.filenames]):
+            self.base.conf.clean_requirements_on_remove = True
+            demands.allow_erasing = True
+            # disable all available repos to delete whole dependency tree
+            # instead of replacing removable package with available packages
+            demands.available_repos = False
+        else:
+            demands.available_repos = True
+            demands.fresh_metadata = False
 
     def run(self):
-        base = self.base
-        pkgs = base.sack.query()._unneeded(base.sack, base._yumdb,
-                                          debug_solver=base.conf.debug_solver)
-        for pkg in pkgs:
-            base.package_remove(pkg)
+        if any([self.opts.grp_specs, self.opts.pkg_specs, self.opts.filenames]):
+            self.opts.pkg_specs += self.opts.filenames
+            done = False
+            # Remove groups.
+            if self.opts.grp_specs and self.forms:
+                for grp_spec in self.opts.grp_specs:
+                    msg = _('Not a valid form: %s')
+                    logger.warning(msg, self.base.output.term.bold(grp_spec))
+            elif self.opts.grp_specs:
+                self.base.read_comps(arch_filter=True)
+                if self.base.env_group_remove(self.opts.grp_specs):
+                    done = True
+
+            for pkg_spec in self.opts.pkg_specs:
+                try:
+                    self.base.remove(pkg_spec, forms=self.forms)
+                except dnf.exceptions.MarkingError:
+                    logger.info(_('No match for argument: %s'),
+                                pkg_spec)
+                else:
+                    done = True
+
+            if not done:
+                raise dnf.exceptions.Error(_('No packages marked for removal.'))
+
+        else:
+            base = self.base
+            pkgs = base.sack.query()._unneeded(base.sack, base._yumdb,
+                                               debug_solver=base.conf.debug_solver)
+            for pkg in pkgs:
+                base.package_remove(pkg)
