c6fb1e0399cd7219bdd3cca2637c26e816d738cf
Jaroslav Mracek <jmracek@redhat.com>
2016-12-09 09:57:35 +0100

    Change warning about added repo into info (RhBug:1243393)
    Revert: 3326f77aa9d33f9cec948ce148d63ed3e5084b83

--- dnf/cli/cli.py
+++ dnf/cli/cli.py
@@ -688,7 +688,7 @@ class Cli(object):
                 except ValueError as e:
                     raise dnf.exceptions.RepoError(e)
                 self.base.repos.add(repofp)
-                logger.warning(_("Added %s repo from %s"), label, path)
+                logger.info(_("Added %s repo from %s"), label, path)
 
                 # do not let this repo to be disabled
                 opts.repos_ed.append((label, "enable"))
