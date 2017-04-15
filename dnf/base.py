rpm-software-management/dnf

commit 55085bdab3609802705668b6098488036f1487f2

Mark installonlypkgs correctly as user installed (RhBug:1349314)
It recognized packages as install_only by name and not by provide. It was fixed
by this patch.

https://bugzilla.redhat.com/show_bug.cgi?id=1349314

Closes: #775
Approved by: ignatenkobrain

dnf/base.py
@@ -899,8 +899,7 @@ class Base(object):
             yumdb_info = self._yumdb.get_package(po)
             yumdb_info.from_repo = rpo.repoid
 
-            yumdb_info.reason = tsi._propagated_reason(self._yumdb,
-                                                      self.conf.installonlypkgs)
+            yumdb_info.reason = tsi._propagated_reason(self._yumdb, self._get_installonly_query())
             yumdb_info.releasever = self.conf.releasever
             if hasattr(self, 'args') and self.args:
                 yumdb_info.command_line = ' '.join(self.args)

dnf/transaction.py
@@ -98,10 +98,10 @@ class TransactionItem(object):
     def _obsoleting_history_state(self):
         return 'Obsoleting'
 
-    def _propagated_reason(self, yumdb, installonlypkgs):
+    def _propagated_reason(self, yumdb, installonlypkgs_query):
         if self.reason == 'user':
             return self.reason
-        if self.installed.name in installonlypkgs:
+        if installonlypkgs_query.filter(name=self.installed.name):
             return 'user'
         if self.op_type in [DOWNGRADE, REINSTALL, UPGRADE]:
             previously = yumdb.get_package(self.erased).get('reason')

tests/test_transaction.py
@@ -94,22 +94,27 @@ class TransactionItemTest(tests.support.TestCase):
     def test_propagated_reason(self):
         yumdb = mock.Mock()
         yumdb.get_package().get = lambda s: 'dep'
-
+        self.base = tests.support.BaseCliStub()
+        self.base._sack = tests.support.mock_sack('main', 'search')
         tsi = dnf.transaction.TransactionItem(
             dnf.transaction.INSTALL, installed=self.newpkg, reason='user')
-        self.assertEqual(tsi._propagated_reason(yumdb, []), 'user')
+        self.assertEqual(tsi._propagated_reason(yumdb, self.base._sack.query().filter(empty=True)),
+                         'user')
         tsi = dnf.transaction.TransactionItem(
             dnf.transaction.UPGRADE, installed=self.newpkg, erased=self.oldpkg)
-        self.assertEqual(tsi._propagated_reason(yumdb, []), 'dep')
+        self.assertEqual(tsi._propagated_reason(yumdb, self.base._sack.query().filter(empty=True)),
+                         'dep')
         tsi = dnf.transaction.TransactionItem(
             dnf.transaction.DOWNGRADE,
             installed=self.newpkg, erased=self.oldpkg)
-        self.assertEqual(tsi._propagated_reason(yumdb, []), 'dep')
+        self.assertEqual(tsi._propagated_reason(yumdb, self.base._sack.query().filter(empty=True)),
+                         'dep')
 
         # test the call can survive if no reason is known:
         yumdb = mock.Mock()
         yumdb.get_package().get = lambda s: None
-        self.assertEqual(tsi._propagated_reason(yumdb, []), 'unknown')
+        self.assertEqual(tsi._propagated_reason(yumdb, self.base._sack.query().filter(empty=True)),
+                         'unknown')
 
     def test_removes(self):
         tsi = dnf.transaction.TransactionItem(

commit 30381480f549928a9b3606514daf7e0dcd194c92

[swdb]: obsoleted reason propagation + installonly
Upstream d547084 and 55085bd broke swdb when rebasing.
Due to amount of commits in SWDB PR it's easier to fix it this way.

dnf/base.py
@@ -761,8 +761,10 @@ class Base(object):
                 cmdline = ' '.join(self.cmds)
 
             tsis = list(self.transaction)
+            installonly = self._get_installonly_query()
+
             for tsi in tsis:
-                tsi._propagate_reason(self.history, self.conf.installonlypkgs)
+                tsi._propagate_reason(self.history, installonly)
 
             self.history.beg(rpmdbv, using_pkgs, tsis, cmdline)
             # write out our config and repo data to additional history info

dnf/transaction.py
@@ -100,10 +100,10 @@ class TransactionItem(object):
     def _obsoleting_history_state(self):
         return 'Obsoleting'
 
-    def _propagated_reason(self, history, installonlypkgs):
+    def _propagated_reason(self, history, installonly):
         if self.reason == 'user':
             return self.reason
-        if self.installed and installonlypkgs_query.filter(name=self.installed.name):
+        if self.installed and installonly.filter(name=self.installed.name):
             return 'user'
         if self.op_type in self._HISTORY_ERASE and self.erased:
             previously = history.reason_by_nvra(self.erased)
@@ -112,7 +112,7 @@ class TransactionItem(object):
         if self.obsoleted:
             reasons = set()
             for obs in self.obsoleted:
-                reasons.add(yumdb.get_package(obs).get('reason'))
+                reasons.add(history.reason_by_nvra(obs))
             if reasons:
                 if 'user' in reasons:
                     return 'user'

dnf/yum/swdb_transformer.py
@@ -287,7 +287,6 @@ def run(input_dir='/var/lib/dnf/',
         sys.stderr.write('FAIL: aborting SWDB transformer\n')
         return False
 
-
     # value distribution in tables
     PACKAGE_DATA = ['P_ID', 'R_ID', 'from_repo_revision',
                     'from_repo_timestamp', 'installed_by', 'changed_by',

tests/test_transaction.py
@@ -94,6 +94,7 @@ class TransactionItemTest(tests.support.TestCase):
     def test_propagated_reason(self):
         base = tests.support.MockBase()
         history = base.history
+        base._sack = tests.support.mock_sack('main', 'search')
 
         pkg1 = history.ipkg_to_pkg(self.newpkg)
         pid = history.add_package(pkg1)
@@ -103,20 +104,22 @@ class TransactionItemTest(tests.support.TestCase):
         pid = history.add_package(pkg2)
         history.swdb.trans_data_beg(1, pid, "dep", "Installed")
 
+        ionly = base._sack.query().filter(empty=True)  # installonly_query
+
         tsi = dnf.transaction.TransactionItem(
             dnf.transaction.INSTALL, installed=self.newpkg, reason='user')
-        self.assertEqual(tsi._propagated_reason(history, []), 'user')
+        self.assertEqual(tsi._propagated_reason(history, ionly), 'user')
         tsi = dnf.transaction.TransactionItem(
             dnf.transaction.UPGRADE, installed=self.newpkg, erased=self.oldpkg)
-        self.assertEqual(tsi._propagated_reason(history, []), 'dep')
+        self.assertEqual(tsi._propagated_reason(history, ionly), 'dep')
         tsi = dnf.transaction.TransactionItem(
             dnf.transaction.DOWNGRADE,
             installed=self.newpkg, erased=self.oldpkg)
-        self.assertEqual(tsi._propagated_reason(history, []), 'dep')
+        self.assertEqual(tsi._propagated_reason(history, ionly), 'dep')
 
         # test the call can survive if no reason is known:
         history.reset_db()
-        self.assertEqual(tsi._propagated_reason(history, []), 'unknown')
+        self.assertEqual(tsi._propagated_reason(history, ionly), 'unknown')
 
     def test_removes(self):
         tsi = dnf.transaction.TransactionItem(