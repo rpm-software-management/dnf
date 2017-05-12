rpm-software-management/dnf

commit d100a29dc46fe4e849ea1d5c0a4632c6b6d40332

removed other redundant functions to convert args to transaction ids
Closes: #659
Approved by: mluscon

dnf/cli/commands/__init__.py
@@ -938,7 +938,7 @@ class HistoryCommand(Command):
         elif vcmd == 'list':
             ret = self.output.historyListCmd(extcmds)
         elif vcmd == 'info':
-            ret = self.output.historyInfoCmd(extcmds)
+            ret = self.output.historyInfoCmd(extcmds, self.opts.tid)
         elif vcmd == 'undo':
             ret = self._hcmd_undo(extcmds)
         elif vcmd == 'redo':

dnf/cli/output.py
@@ -1484,7 +1484,7 @@ Transaction Summary
                     lmark = '>'
                 print(fmt % (old.tid, name, tm, uiacts, num), "%s%s" % (lmark, rmark))
 
-    def historyInfoCmd(self, extcmds):
+    def historyInfoCmd(self, extcmds, pats=[]):
         """Output information about a transaction in history
 
         :param extcmds: list of extra command line arguments
@@ -1495,36 +1495,12 @@ Transaction Summary
             0 = we're done, exit
             1 = we've errored, exit with error string
         """
-        tids = set()
+        tids = extcmds
         mtids = set()
-        pats = []
         old = self.history.last()
         if old is None:
             logger.critical(_('No transactions'))
             return 1, ['Failed history info']
-
-        for tid in extcmds:
-            if self._historyRangeRTIDs(old, tid):
-                # Have a range ... do a "merged" transaction.
-                mtids.add(self._historyRangeRTIDs(old, tid))
-                continue
-
-            try:
-                id_or_offset = self.base.transaction_id_or_offset(tid)
-            except ValueError:
-                # A package pattern.
-                pats.append(tid)
-                continue
-
-            # A transaction ID or an offset from the last transaction ID.
-            tids.add(id_or_offset if id_or_offset >= 0 else
-                     old.tid + id_or_offset + 1)
-        if pats:
-            tids.update(self.history.search(pats))
-        utids = tids.copy()
-        if mtids:
-            mtids = sorted(mtids)
-            tids.update(self._historyRangeTIDs(mtids))
 
         if not tids and len(extcmds) < 2:
             old = self.history.last(complete_transactions_only=False)
@@ -1572,12 +1548,11 @@ Transaction Summary
                     if tid.tid >= bmtid and tid.tid <= emtid:
                         mobj = dnf.yum.history.YumMergedHistoryTransaction(tid)
 
-            if tid.tid in utids:
-                if done:
-                    print("-" * 79)
-                done = True
+            if done:
+                print("-" * 79)
+            done = True
 
-                self._historyInfoCmd(tid, pats)
+            self._historyInfoCmd(tid, pats)
 
         if mobj is not None:
             if done:

dnf/cli/cli.py
@@ -554,50 +554,12 @@ class BaseCli(dnf.Base):
         # otherwise, don't prompt
         return False
 
-    @staticmethod
-    def transaction_id_or_offset(extcmd):
-        """Convert user input to a transaction ID or an offset from the end."""
-        try:
-            offset_str, = re.match('^--last(-\d+)?$', extcmd).groups()
-        except AttributeError:  # extcmd does not match the regex.
-            id_ = int(extcmd)
-            if id_ < 0:
-                # Negative return values are reserved for offsets.
-                raise ValueError('bad transaction ID given: %s' % extcmd)
-            return id_
-        else:
-            # Was extcmd '--last-N' or just '--last'?
-            offset = int(offset_str) if offset_str else 0
-            # Return offsets as negative numbers, where -1 means the last
-            # transaction as when indexing sequences.
-            return offset - 1
-
     def _history_get_transactions(self, extcmds):
         if not extcmds:
             logger.critical(_('No transaction ID given'))
             return None
 
-        tids = []
-        last = None
-        for extcmd in extcmds:
-            try:
-                id_or_offset = self.transaction_id_or_offset(extcmd)
-            except ValueError:
-                logger.critical(_('Bad transaction ID given'))
-                return None
-
-            if id_or_offset < 0:
-                if last is None:
-                    cto = False
-                    last = self.history.last(complete_transactions_only=cto)
-                    if last is None:
-                        logger.critical(_('Bad transaction ID given'))
-                        return None
-                tids.append(str(last.tid + id_or_offset + 1))
-            else:
-                tids.append(str(id_or_offset))
-
-        old = self.history.old(tids)
+        old = self.history.old(extcmds)
         if not old:
             logger.critical(_('Not found given transaction ID'))
             return None

commit d7bbd18984869d1844911aa86e7832a65b4e1de8

[history]: fixed info command merged output
dnf history info merged output was broken since d100a29

reintroduced this functionality to keep yum-dnf compatibility

dnf/cli/commands/__init__.py
@@ -896,7 +896,7 @@ class HistoryCommand(Command):
         pkgs = tuple(self.base.iter_userinstalled())
         return self.output.listPkgs(pkgs, 'Packages installed by user', 'nevra')
 
-    def _convert_tids(self):
+    def _convert_tids(self, merged=set()):
         """Convert commandline arguments to transaction ids"""
         def str2tid(s):
             if s.startswith('last'):
@@ -916,6 +916,7 @@ class HistoryCommand(Command):
                     tmp = btid
                     btid = etid
                     etid = tmp
+                merged.add((btid, etid))
                 tids.update(range(btid, etid + 1))
             else:
                 try:
@@ -928,12 +929,13 @@ class HistoryCommand(Command):
 
     def run(self):
         vcmd = self.opts.tid_action
-        extcmds = self._convert_tids()
+        merged = set()
+        extcmds = self._convert_tids(merged)
 
         if vcmd == 'list':
             ret = self.output.historyListCmd(extcmds)
         elif vcmd == 'info':
-            ret = self.output.historyInfoCmd(extcmds, self.opts.tid)
+            ret = self.output.historyInfoCmd(extcmds, self.opts.tid, merged)
         elif vcmd == 'undo':
             ret = self._hcmd_undo(extcmds)
         elif vcmd == 'redo':

dnf/cli/output.py
@@ -1485,7 +1485,7 @@ Transaction Summary
                     lmark = '>'
                 print(fmt % (old.tid, name, tm, uiacts, num), "%s%s" % (lmark, rmark))
 
-    def historyInfoCmd(self, extcmds, pats=[]):
+    def historyInfoCmd(self, extcmds, pats=[], mtids=set()):
         """Output information about a transaction in history
 
         :param extcmds: list of extra command line arguments
@@ -1496,33 +1496,32 @@ Transaction Summary
             0 = we're done, exit
             1 = we've errored, exit with error string
         """
-        tids = extcmds
-        mtids = set()
+        tids = set(extcmds)
         old = self.history.last()
         if old is None:
             logger.critical(_('No transactions'))
             return 1, ['Failed history info']
 
+        lasttid = old.tid
+        lastdbv = old.end_rpmdbversion
+
         if not tids and len(extcmds) < 2:
             old = self.history.last(complete_transactions_only=False)
             if old is not None:
                 tids.add(old.tid)
-                utids.add(old.tid)
 
         if not tids:
             logger.critical(_('No transaction ID, or package, given'))
             return 1, ['Failed history info']
 
-        lastdbv = self.history.last()
-        if lastdbv is not None:
-            lasttid = lastdbv.tid
-            lastdbv = lastdbv.end_rpmdbversion
-
-        done = False
         bmtid, emtid = -1, -1
         mobj = None
+        done = False
+
         if mtids:
-            bmtid, emtid = mtids.pop(0)
+            mtids = sorted(mtids)
+            bmtid, emtid = mtids.pop()
+
         for tid in self.history.old(tids):
             if lastdbv is not None and tid.tid == lasttid:
                 #  If this is the last transaction, is good and it doesn't
@@ -1532,11 +1531,14 @@ Transaction Summary
                     tid.altered_gt_rpmdb = True
             lastdbv = None
 
+            merged = False
+
             if tid.tid >= bmtid and tid.tid <= emtid:
                 if mobj is None:
                     mobj = dnf.yum.history.YumMergedHistoryTransaction(tid)
                 else:
                     mobj.merge(tid)
+                merged = True
             elif mobj is not None:
                 if done:
                     print("-" * 79)
@@ -1545,20 +1547,20 @@ Transaction Summary
                 self._historyInfoCmd(mobj)
                 mobj = None
                 if mtids:
-                    bmtid, emtid = mtids.pop(0)
+                    bmtid, emtid = mtids.pop()
                     if tid.tid >= bmtid and tid.tid <= emtid:
                         mobj = dnf.yum.history.YumMergedHistoryTransaction(tid)
+                        merged = True
 
-            if done:
-                print("-" * 79)
-            done = True
-
-            self._historyInfoCmd(tid, pats)
+            if not merged:
+                if done:
+                    print("-" * 79)
+                done = True
+                self._historyInfoCmd(tid, pats)
 
         if mobj is not None:
             if done:
                 print("-" * 79)
-
             self._historyInfoCmd(mobj)
 
     def _hpkg2from_repo(self, hpkg):