b490586e0c0ca730703d49919b425dc2a133fe77
Jaroslav Mracek <jmracek@redhat.com>
2017-05-17 17:07:37 +0200

    Report excludes includes into logger.debug (RhBug:1381988)
    It reports separately this information for each repo and general excludes in
    dnf.conf. If excludes or includes are set from commandline they are recognized
    as they origin from .repo of .conf source.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1381988
    
    Closes: #814
    Approved by: dmach

--- dnf/cli/cli.py
+++ dnf/cli/cli.py
@@ -155,6 +155,19 @@ class BaseCli(dnf.Base):
            occurred in the pre-transaction checks
         """
 
+        # Reports about excludes and includes (but not from plugins)
+        if self.conf.excludepkgs:
+            logger.debug('Excludes in dnf.conf: ' + ", ".join(sorted(set(self.conf.excludepkgs))))
+        if self.conf.includepkgs:
+            logger.debug('Includes in dnf.conf: ' + ", ".join(sorted(set(self.conf.includepkgs))))
+        for repo in self.repos.iter_enabled():
+            if repo.excludepkgs:
+                logger.debug(
+                    'Excludes in repo ' + repo.id + ": " + ", ".join(sorted(set(repo.excludepkgs))))
+            if repo.includepkgs:
+                logger.debug(
+                    'Includes in repo ' + repo.id + ": " + ", ".join(sorted(set(repo.includepkgs))))
+
         grp_diff = self._groups_diff()
         grp_str = self.output.list_group_transaction(self.comps, self._group_persistor, grp_diff)
         if grp_str:
