44b51695b347104920e20baa4c979b9410cd23c9
Jaroslav Mracek <jmracek@redhat.com>
2017-06-13 19:06:07 +0200

    Inform about skipped packages for group install (RhBug:1427365)
    Each group member that have no match in sack will be reported by warning.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1427365

--- dnf/base.py
+++ dnf/base.py
@@ -1381,8 +1381,15 @@ class Base(object):
                 if (pkg.basearchonly):
                     query_args.update({'arch': basearch})
                 q = self.sack.query().filter(**query_args).apply()
-                if not q or not cond_check(pkg):
-                    # a conditional package with unsatisfied requiremensts
+                if not q:
+                    package_string = pkg.name
+                    if pkg.basearchonly:
+                        package_string = package_string + '.' + basearch
+                    msg = _('No match for group package "{}"')
+                    logger.warning(msg.format(package_string))
+                    continue
+                # a conditional package with unsatisfied requiremensts
+                if not cond_check(pkg):
                     continue
                 q = q.filter(arch__neq="src")
                 if self.conf.multilib_policy == "all":
