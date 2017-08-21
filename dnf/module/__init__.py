9645934d7ffc3e3e9db687f743d7bb9e49c507a6
Martin Hatina <mhatina@redhat.com>
2017-08-21 10:27:23 +0200

    refactor modularity part

--- dnf/module/__init__.py
+++ dnf/module/__init__.py
@@ -14,6 +14,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+from dnf.i18n import _
+
 LOAD_CACHE_ERR = 1
 MISSING_YAML_ERR = 2
 NO_METADATA_ERR = 3
@@ -31,23 +33,31 @@ PARSING_ERR = 14
 PROFILE_NOT_INSTALLED = 15
 VERSION_LOCKED = 16
 INSTALLING_NEWER_VERSION = 17
+NO_PROFILE_SPECIFIED = 18
+POSSIBLE_PROFILES = 19
+NO_PROFILES_AVAILABLE = 20
+NO_DEFAULT_STREAM_ERR = 21
 
 module_errors = {
-    LOAD_CACHE_ERR: "Cannot load from cache dir: {}",
-    MISSING_YAML_ERR: "Missing file *modules.yaml in metadata cache dir: {}",
-    NO_METADATA_ERR: "No available metadata for module: {}",
-    NO_MODULE_OR_STREAM_ERR: "No such module: {} or active stream (enable a stream first)",
-    NO_MODULE_ERR: "No such module: {}",
-    NO_PROFILE_ERR: "No such profile: {}. Possible profiles: {}",
-    NO_STREAM_ERR: "No such stream {} in {}",
-    NO_ACTIVE_STREAM_ERR: "No active stream for module: {}",
-    STREAM_NOT_ENABLED_ERR: "Stream not enabled. Skipping {}",
-    DIFFERENT_STREAM_INFO: "Enabling different stream for {}",
-    INVALID_MODULE_ERR: "Not a valid module: {}",
-    LOWER_VERSION_INFO: "Using lower version due to missing profile in latest version",
-    NOTHING_TO_SHOW: "Nothing to show",
-    PARSING_ERR: "Probable parsing problem of {}, try specifying MODULE-STREAM-VERSION",
-    PROFILE_NOT_INSTALLED: "Profile not installed: {}",
-    VERSION_LOCKED: "'{}' is locked to version: {}",
-    INSTALLING_NEWER_VERSION: "Installing newer version of '{}' than specified. Reason: {}",
+    LOAD_CACHE_ERR: _("Cannot load from cache dir: {}"),
+    MISSING_YAML_ERR: _("Missing file *modules.yaml in metadata cache dir: {}"),
+    NO_METADATA_ERR: _("No available metadata for module: {}"),
+    NO_MODULE_OR_STREAM_ERR: _("No such module: {} or active stream (enable a stream first)"),
+    NO_MODULE_ERR: _("No such module: {}"),
+    NO_PROFILE_ERR: _("No such profile: {}."),
+    NO_STREAM_ERR: _("No such stream '{}' in '{}'"),
+    NO_ACTIVE_STREAM_ERR: _("No active stream for module: {}"),
+    STREAM_NOT_ENABLED_ERR: _("Stream not enabled. Skipping '{}'"),
+    DIFFERENT_STREAM_INFO: _("Enabling different stream for '{}'"),
+    INVALID_MODULE_ERR: _("Not a valid module: {}"),
+    LOWER_VERSION_INFO: _("Using lower version due to missing profile in latest version"),
+    NOTHING_TO_SHOW: _("Nothing to show"),
+    PARSING_ERR: _("Probable parsing problem of '{}', try specifying MODULE-STREAM-VERSION"),
+    PROFILE_NOT_INSTALLED: _("Profile not installed: {}"),
+    VERSION_LOCKED: _("'{}' is locked to version: {}"),
+    INSTALLING_NEWER_VERSION: _("Installing newer version of '{}' than specified. Reason: {}"),
+    NO_PROFILE_SPECIFIED: _("No profile specified for '{}', please specify profile."),
+    POSSIBLE_PROFILES: _("Possible profiles: {}"),
+    NO_PROFILES_AVAILABLE: _("No profiles available."),
+    NO_DEFAULT_STREAM_ERR: _("No stream specified for '{}', please specify stream.")
 }
