affc44b1979d8d8b08103300dda2461ef9fcd05d
Ville Skyttä <ville.skytta@iki.fi>
2017-09-05 10:15:21 +0300

    Spelling fix

--- dnf/cli/main.py
+++ dnf/cli/main.py
@@ -127,7 +127,7 @@ def cli_run(cli, base):
                 msg = _("(try to add '%s' to command line to replace conflicting "
                         "packages") % "--allowerasing"
                 if cli.base.conf.strict:
-                    msg += _(" or '%s' to skip uninstalable packages)") % "--skip-broken"
+                    msg += _(" or '%s' to skip uninstallable packages)") % "--skip-broken"
                 else:
                     msg += ")"
                 logger.info(msg)
