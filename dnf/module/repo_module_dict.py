bf6b90aea6531914a4a548ffb46996dfe2b6594e
Martin Hatina <mhatina@redhat.com>
2017-09-06 09:36:33 +0200

    split module list by repo

--- dnf/module/repo_module_dict.py
+++ dnf/module/repo_module_dict.py
@@ -483,7 +483,6 @@ class RepoModuleDict(OrderedDict):
 
         table = smartcols.Table()
         table.maxout = True
-
         column_name = table.new_column("Name")
         column_stream = table.new_column("Stream")
         column_version = table.new_column("Version")
