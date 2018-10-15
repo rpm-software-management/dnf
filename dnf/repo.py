bdc25f007edbfc15dc37dec116829c2f6ed56579
Marek Blaha <mblaha@redhat.com>
2018-10-15 14:50:18 +0200

    [repo] Set substitution attribute of C++ object (RhBug:1637148)
    https://bugzilla.redhat.com/show_bug.cgi?id=1637148

--- dnf/repo.py
+++ dnf/repo.py
@@ -436,6 +436,7 @@ class Repo(dnf.conf.RepoConf):
         self._key_import = _NullKeyImport()
         self.metadata = None  # :api
         self._repo.setSyncStrategy(self.DEFAULT_SYNC)
+        self._repo.setSubstitutions(parent_conf.substitutions)
         self._substitutions = dnf.conf.substitutions.Substitutions()
         self._hawkey_repo = self._init_hawkey_repo()
         self._check_config_file_age = parent_conf.check_config_file_age \
