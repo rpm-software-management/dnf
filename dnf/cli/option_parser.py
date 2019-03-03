rpm-software-management/dnf

commit 6c45861ad7f5e6a6d586025a05c39b4b7a180aa0

Add missing --bz and --cve command line options
The --bz and --cve are different names for the --bzs and --cves options
already present.

They worked already due to Python argparse's argument abbreviations
feature and are documented in in the man page. List them explicitly
here.

dnf/cli/option_parser.py
@@ -312,10 +312,10 @@ class OptionParser(argparse.ArgumentParser):
                                  default=[], action="append",
                                  help=_("Include packages needed to fix the "
                                         "given advisory, in updates"))
-        main_parser.add_argument("--bzs", default=[], dest="bugzilla",
+        main_parser.add_argument("--bz", "--bzs", default=[], dest="bugzilla",
                                  action="append", help=_(
                 "Include packages needed to fix the given BZ, in updates"))
-        main_parser.add_argument("--cves", default=[], action="append", help=_(
+        main_parser.add_argument("--cve", "--cves", default=[], action="append", help=_(
             "Include packages needed to fix the given CVE, in updates"))
         main_parser.add_argument(
             "--sec-severity", "--secseverity",

commit 442dd0ebeb9cd4383989f089f10888d7477e531e

Fix minor issue with option parser
The issue is introduced by 6c45861ad7f5e6a6d586025a05c39b4b7a180aa0

dnf/cli/option_parser.py
@@ -315,8 +315,8 @@ class OptionParser(argparse.ArgumentParser):
         main_parser.add_argument("--bz", "--bzs", default=[], dest="bugzilla",
                                  action="append", help=_(
                 "Include packages needed to fix the given BZ, in updates"))
-        main_parser.add_argument("--cve", "--cves", default=[], action="append", help=_(
-            "Include packages needed to fix the given CVE, in updates"))
+        main_parser.add_argument("--cve", "--cves", default=[], dest="cves", action="append",
+                                 help=_("Include packages needed to fix the given CVE, in updates"))
         main_parser.add_argument(
             "--sec-severity", "--secseverity",
             choices=['Critical', 'Important', 'Moderate', 'Low'], default=[],