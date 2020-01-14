rpm-software-management/dnf

commit bd00c044ef188688b4c4ec6116ac039327fb6466

Accept multiple specs in repoquery options (RhBug:1667898)
It allows with repoquery command to use --what* options multiple times
(append option) or add multiple arguments separated by comma.

https://bugzilla.redhat.com/show_bug.cgi?id=1667898

dnf/cli/commands/repoquery.py
@@ -126,24 +126,33 @@ class RepoQueryCommand(commands.Command):
                             help=_('show only results from this ARCH'))
         parser.add_argument('-f', '--file', metavar='FILE', nargs='+',
                             help=_('show only results that owns FILE'))
-        parser.add_argument('--whatconflicts', metavar='REQ',
+        parser.add_argument('--whatconflicts', default=[], action=OptionParser._SplitCallback,
+                            metavar='REQ',
                             help=_('show only results that conflict REQ'))
-        parser.add_argument('--whatdepends', metavar='REQ',
+        parser.add_argument('--whatdepends', default=[], action=OptionParser._SplitCallback,
+                            metavar='REQ',
                             help=_('shows results that requires, suggests, supplements, enhances,'
                                    'or recommends package provides and files REQ'))
-        parser.add_argument('--whatobsoletes', metavar='REQ',
+        parser.add_argument('--whatobsoletes', default=[], action=OptionParser._SplitCallback,
+                            metavar='REQ',
                             help=_('show only results that obsolete REQ'))
-        parser.add_argument('--whatprovides', metavar='REQ',
+        parser.add_argument('--whatprovides', default=[], action=OptionParser._SplitCallback,
+                            metavar='REQ',
                             help=_('show only results that provide REQ'))
-        parser.add_argument('--whatrequires', metavar='REQ',
+        parser.add_argument('--whatrequires', default=[], action=OptionParser._SplitCallback,
+                            metavar='REQ',
                             help=_('shows results that requires package provides and files REQ'))
-        parser.add_argument('--whatrecommends', metavar='REQ',
+        parser.add_argument('--whatrecommends', default=[], action=OptionParser._SplitCallback,
+                            metavar='REQ',
                             help=_('show only results that recommend REQ'))
-        parser.add_argument('--whatenhances', metavar='REQ',
+        parser.add_argument('--whatenhances', default=[], action=OptionParser._SplitCallback,
+                            metavar='REQ',
                             help=_('show only results that enhance REQ'))
-        parser.add_argument('--whatsuggests', metavar='REQ',
+        parser.add_argument('--whatsuggests', default=[], action=OptionParser._SplitCallback,
+                            metavar='REQ',
                             help=_('show only results that suggest REQ'))
-        parser.add_argument('--whatsupplements', metavar='REQ',
+        parser.add_argument('--whatsupplements', default=[], action=OptionParser._SplitCallback,
+                            metavar='REQ',
                             help=_('show only results that supplement REQ'))
         whatrequiresform = parser.add_mutually_exclusive_group()
         whatrequiresform.add_argument("--alldeps", action="store_true",
@@ -354,10 +363,12 @@ class RepoQueryCommand(commands.Command):
         return t.union(done)
 
     def by_all_deps(self, requires_name, depends_name, query):
-        name = requires_name or depends_name
-        defaultquery = query.intersection(dnf.subject.Subject(name).get_best_query(
-            self.base.sack, with_provides=False, with_filenames=False))
-        requiresquery = query.filter(requires__glob=name)
+        names = requires_name or depends_name
+        defaultquery = self.base.sack.query().filter(empty=True)
+        for name in names:
+            defaultquery.union(query.intersection(dnf.subject.Subject(name).get_best_query(
+                self.base.sack, with_provides=False, with_filenames=False)))
+        requiresquery = query.filter(requires__glob=names)
         if depends_name:
             requiresquery = requiresquery.union(query.filter(recommends__glob=depends_name))
             requiresquery = requiresquery.union(query.filter(enhances__glob=depends_name))
@@ -446,7 +457,7 @@ class RepoQueryCommand(commands.Command):
         if self.opts.whatobsoletes:
             q.filterm(obsoletes=self.opts.whatobsoletes)
         if self.opts.whatprovides:
-            query_for_provide = q.filter(provides__glob=[self.opts.whatprovides])
+            query_for_provide = q.filter(provides__glob=self.opts.whatprovides)
             if query_for_provide:
                 q = query_for_provide
             else:

dnf.spec
@@ -1,5 +1,5 @@
 # default dependencies
-%global hawkey_version 0.35.1
+%global hawkey_version 0.35.2
 %global libcomps_version 0.1.8
 %global libmodulemd_version 1.4.0
 %global rpm_version 4.14.0

tests/test_repoquery.py
@@ -78,8 +78,8 @@ class ArgParseTest(tests.support.TestCase):
 
     def test_parse(self):
         tests.support.command_configure(self.cmd, ['--whatrequires', 'prudence'])
-        self.assertIsNone(self.cmd.opts.whatprovides)
-        self.assertEqual(self.cmd.opts.whatrequires, 'prudence')
+        self.assertEqual(self.cmd.opts.whatprovides, [])
+        self.assertEqual(self.cmd.opts.whatrequires, ['prudence'])
         self.assertEqual(self.cmd.opts.queryformat,
                          dnf.cli.commands.repoquery.QFORMAT_DEFAULT)
 

commit d9492fef24989085011047dd561fef1dfa1e31d6

repoquery: fix calling by_all_deps() for --tree
Fixes a bug introduced in bd00c044, where the by_all_deps() arguments
were changed to an array, but in tree_seed() it still passes a single
string.

Untested, I have actually no idea what the code does there, it looks
kind of suspect.

dnf/cli/commands/repoquery.py
@@ -674,7 +674,7 @@ class RepoQueryCommand(commands.Command):
                             ar[querypkg.name + "." + querypkg.arch] = querypkg
                     pkgquery = self.base.sack.query().filterm(pkg=list(ar.values()))
                 else:
-                    pkgquery = self.by_all_deps(pkg.name, None, aquery) if opts.alldeps \
+                    pkgquery = self.by_all_deps((pkg.name, ), None, aquery) if opts.alldeps \
                         else aquery.filter(requires__glob=pkg.name)
                 self.tree_seed(pkgquery, aquery, opts, level + 1, usedpkgs)
 