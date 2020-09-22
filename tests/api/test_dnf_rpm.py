95bd17c668d2a4b69eb78d141121c386833cd973
Aleš Matěj <amatej@redhat.com>
2020-09-22 12:49:13 +0200

    tests: API tests for dnf.rpm
    https://issues.redhat.com/browse/RHELPLAN-49988

---
+++ tests/api/test_dnf_rpm.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import dnf
+
+from .common import TestCase
+
+
+class DnfRpmApiTest(TestCase):
+    def test_detect_releasever(self):
+        # dnf.rpm.detect_releasever
+        self.assertHasAttr(dnf.rpm, "detect_releasever")
+        self.assertHasType(dnf.rpm.detect_releasever(installroot='/'), str)
+
+    def test_basearch(self):
+        # dnf.rpm.basearch
+        self.assertHasAttr(dnf.rpm, "basearch")
+        self.assertHasType(dnf.rpm.basearch(arch="x86_64"), str)
