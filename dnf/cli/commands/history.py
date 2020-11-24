acfd6310131769f33165c8de1d064889a80fc259
Daniel Mach <dmach@redhat.com>
2020-11-24 10:57:21 +0100

    transaction_sr: Enable loading transactions from dict

--- dnf/cli/commands/history.py
+++ dnf/cli/commands/history.py
@@ -270,7 +270,7 @@ class HistoryCommand(commands.Command):
 
             self.replay = TransactionReplay(
                 self.base,
-                self.opts.transaction_filename,
+                filename=self.opts.transaction_filename,
                 ignore_installed = self.opts.ignore_installed,
                 ignore_extras = self.opts.ignore_extras,
                 skip_unavailable = self.opts.skip_unavailable
