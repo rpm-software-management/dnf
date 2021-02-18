88268c69aa925eea6b03cf67d09e4a607c20366d
Pavla Kratochvilova <pkratoch@redhat.com>
2021-02-18 13:36:50 +0100

    Replace python-nose with unittest
    This also needed setting locale for testing, because for some reason,
    the default locale is different when running unittest instead of
    python-nose.
    
    = changelog =
    resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1916783

--- tests/__init__.py
+++ tests/__init__.py
@@ -17,9 +17,11 @@
 # Red Hat, Inc.
 #
 
+import locale
 import os
 
 # run tests with C locales
 os.environ["LC_ALL"] = "C.UTF-8"
 os.environ["LANG"] = "C.UTF-8"
 os.environ["LANGUAGE"] = "en_US:en"
+locale.setlocale(locale.LC_ALL, "C.UTF-8")
