734aab779bfd6c1792dd17528b30215a715fa898
Joseph Marrero <jmarrero@redhat.com>
2024-07-16 15:48:41 -0400

    Update ostree/bootc host system check.
    Upstream commit: 6120fe52511775b60b6031d4169988c025610ab5
    
    This changes the is_container() func for _is_bootc_host()
    and updates the logic and message. This should detect on
    all ostree and bootc hosts to date that are not using
    bootc usroverlay or ostree admin unlock for development
    purposes.
    
    Resolves: https://issues.redhat.com/browse/RHEL-49670

--- dnf/cli/cli.py
+++ dnf/cli/cli.py
@@ -214,13 +214,12 @@ class BaseCli(dnf.Base):
             elif 'test' in self.conf.tsflags:
                 logger.info(_("{prog} will only download packages, install gpg keys, and check the "
                               "transaction.").format(prog=dnf.util.MAIN_PROG_UPPER))
-            if dnf.util.is_container():
-                _container_msg = _("""
-*** This system is managed with ostree.  Changes to the system
-*** made with dnf will be lost with the next ostree-based update.
-*** If you do not want to lose these changes, use 'rpm-ostree'.
+            if dnf.util._is_bootc_host():
+                _bootc_host_msg = _("""
+*** Error: system is configured to be read-only; for more
+*** information run `bootc status` or `ostree admin status`.
 """)
-                logger.info(_container_msg)
+                logger.info(_bootc_host_msg)
                 raise CliError(_("Operation aborted."))
 
             if self._promptWanted():
