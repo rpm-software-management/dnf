rpm-software-management/dnf

commit ab1a0d54d25880b431dcad71eb5eaf0410a8b0fc

spec: Allow to build with ninja
For
https://discussion.fedoraproject.org/t/f43-change-proposal-cmake-use-ninja-generator-by-default-system-wide/148867.

dnf.spec
@@ -1,6 +1,3 @@
-# Always build out-of-source
-%define __cmake_in_source_build 1
-
 # default dependencies
 %global hawkey_version 0.75.0
 %global libcomps_version 0.1.8
@@ -205,21 +202,13 @@ Additional dependencies needed to perform transactions on booted bootc (bootable
 %prep
 %autosetup
 
-mkdir build-py3
-
 %build
-
-pushd build-py3
-%cmake .. -DPYTHON_DESIRED:FILEPATH=%{__python3} -DDNF_VERSION=%{version}
-%make_build
-make doc-man
-popd
+%cmake -DPYTHON_DESIRED:FILEPATH=%{__python3} -DDNF_VERSION=%{version}
+%cmake_build
+%cmake_build -t doc-man
 
 %install
-
-pushd build-py3
-%make_install
-popd
+%cmake_install
 
 %find_lang %{name}
 mkdir -p %{buildroot}%{confdir}/vars
@@ -293,10 +282,7 @@ rm %{buildroot}%{_unitdir}/%{name}-makecache.timer
 %endif
 
 %check
-
-pushd build-py3
-ctest -VV
-popd
+%ctest -VV
 
 
 %if %{without dnf5_obsoletes_dnf}

commit 4bf455a442390decc433779947bcb547220054f6

spec: Fix executing ctest on RHEL < 10
Commit ab1a0d54d25880b431dcad71eb5eaf0410a8b0fc (spec: Allow to build
with ninja) broke spec parsing on RHEL 9 because %ctest macro is does
not recongize any arguments there:

$ rpm --eval '%ctest -VV'
error: Unknown option V in ctest(:-:)

This patch falls back to a manual ctest invocation on RHEL < 10.

dnf.spec
@@ -282,7 +282,13 @@ rm %{buildroot}%{_unitdir}/%{name}-makecache.timer
 %endif
 
 %check
+%if 0%{?rhel} && 0%{?rhel} < 10
+pushd %{__cmake_builddir}
+ctest -VV
+popd
+%else
 %ctest -VV
+%endif
 
 
 %if %{without dnf5_obsoletes_dnf}